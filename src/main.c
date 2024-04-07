83d24000efb9569739510da2a2a327b21e144b95
Tong tong <100080262+tongtongwang86@users.noreply.github.com>
2024-04-07 21:47:45 +0800

    blink led

--- src/main.c
+++ src/main.c
@@ -10,8 +10,16 @@
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/usb/usbd.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
+
+
+#define SW0_NODE	DT_ALIAS(sw0) 
+#define LED0_NODE DT_ALIAS(led0)
 
 LOG_MODULE_REGISTER(Rev,LOG_LEVEL_DBG);
+
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
              "Console device is not ACM CDC UART device");
@@ -19,6 +27,19 @@ BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
 
 
 int main(void) {
+
+
+  if (!gpio_is_ready_dt(&led)) {
+		return 0;
+	}
+
+int ret;
+
+ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+if (ret < 0) {
+    return;
+}
+
   const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
   uint32_t dtr = 0;
 
@@ -45,6 +66,10 @@ int main(void) {
     LOG_DBG("A log message in debug level");
     LOG_WRN("A log message in warning level!");
     LOG_ERR("A log message in Error level!");
+    ret = gpio_pin_toggle_dt(&led);
+     if (ret < 0) {
+        return;
+    }
     k_sleep(K_SECONDS(1));
   }
 }
