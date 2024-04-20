ac4e5ed0e32d5ab410c80232a87d822c21b3694e
Tong tong <100080262+tongtongwang86@users.noreply.github.com>
2024-04-20 11:53:17 +0800

    minimum code required for cdc acm logging

--- src/main.c
+++ src/main.c
@@ -33,34 +33,9 @@ LOG_MODULE_REGISTER(Rev,LOG_LEVEL_DBG);
 BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
              "Console device is not ACM CDC UART device");
 
-int as5600_refresh(const struct device *dev)
-{
-	int ret;
-    struct sensor_value rot_raw;
-    ret = sensor_sample_fetch_chan(dev,SENSOR_CHAN_ROTATION);
-	if (ret != 0){
-			printk("sample fetch error:,%d\n", ret);
-		}
-    sensor_channel_get(dev,SENSOR_CHAN_ROTATION, &rot_raw);
-	
 
-    return rot_raw.val1;
-}
 
-static void bq274xx_show_values(const char *type, struct sensor_value value)
-{
-	if ((value.val2 < 0) && (value.val1 >= 0)) {
-		value.val2 = -(value.val2);
-		printk("%s: -%d.%06d\n", type, value.val1, value.val2);
-	} else if ((value.val2 > 0) && (value.val1 < 0)) {
-		printk("%s: %d.%06d\n", type, value.val1, value.val2);
-	} else if ((value.val2 < 0) && (value.val1 < 0)) {
-		value.val2 = -(value.val2);
-		printk("%s: %d.%06d\n", type, value.val1, value.val2);
-	} else {
-		printk("%s: %d.%06d\n", type, value.val1, value.val2);
-	}
-}
+
 
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
@@ -97,19 +72,7 @@ int main(void) {
     k_sleep(K_MSEC(100));
   }
 
-//   const struct device *const bq27 = DEVICE_DT_GET_ONE(ti_bq274xx);
-  // const struct device *const bq27 = DEVICE_DT_GET(DT_INST(0,ti_bq274xx));
-  const struct device *const as = DEVICE_DT_GET_ONE(ams_as5600); 
 
-//   if (!device_is_ready(bq27)) {
-// 		printk("Device %s is not ready\n", bq27->name);
-// 		return 0;
-// 	}
-
-  if (as == NULL || !device_is_ready(as)) {
-		printk("as5600 device tree not configured\n");
-		return;
-	}
 
 
 
@@ -165,181 +128,6 @@ if (ret < 0) {
     LOG_WRN("A log message in warning level!");
     LOG_ERR("A log message in Error level!");
 
-
-    // status = sensor_sample_fetch_chan(bq27,
-		// 				  SENSOR_CHAN_GAUGE_VOLTAGE);
-		// if (status < 0) {
-		// 	printk("Unable to fetch the voltage\n");
-		// 	return;
-		// }
-
-		// status = sensor_channel_get(bq27, SENSOR_CHAN_GAUGE_VOLTAGE,
-		// 			    &voltage);
-		// if (status < 0) {
-		// 	printk("Unable to get the voltage value\n");
-		// 	return;
-		// }
-
-		// printk("Voltage: %d.%06dV\n", voltage.val1, voltage.val2);
-
-		// status = sensor_sample_fetch_chan(bq27,
-		// 			       SENSOR_CHAN_GAUGE_AVG_CURRENT);
-		// if (status < 0) {
-		// 	printk("Unable to fetch the Average current\n");
-		// 	return;
-		// }
-
-		// status = sensor_channel_get(bq27, SENSOR_CHAN_GAUGE_AVG_CURRENT,
-		// 			    &current);
-		// if (status < 0) {
-		// 	printk("Unable to get the current value\n");
-		// 	return;
-		// }
-
-		// bq274xx_show_values("Avg Current in Amps", current);
-
-		// status = sensor_sample_fetch_chan(bq27,
-		// 			SENSOR_CHAN_GAUGE_STDBY_CURRENT);
-		// if (status < 0) {
-		// 	printk("Unable to fetch Standby Current\n");
-		// 	return;
-		// }
-
-		// status = sensor_channel_get(bq27,
-		// 			SENSOR_CHAN_GAUGE_STDBY_CURRENT,
-		// 			&current_standby);
-		// if (status < 0) {
-		// 	printk("Unable to get the current value\n");
-		// 	return;
-		// }
-
-		// bq274xx_show_values("Standby Current in Amps", current_standby);
-
-		// status = sensor_sample_fetch_chan(bq27,
-		// 			SENSOR_CHAN_GAUGE_MAX_LOAD_CURRENT);
-		// if (status < 0) {
-		// 	printk("Unable to fetch Max Load Current\n");
-		// 	return;
-		// }
-
-		// status = sensor_channel_get(bq27,
-		// 			SENSOR_CHAN_GAUGE_MAX_LOAD_CURRENT,
-		// 			&current_max_load);
-		// if (status < 0) {
-		// 	printk("Unable to get the current value\n");
-		// 	return;
-		// }
-
-		// bq274xx_show_values("Max Load Current in Amps",
-		// 		    current_max_load);
-
-		// status = sensor_sample_fetch_chan(bq27,
-		// 			SENSOR_CHAN_GAUGE_STATE_OF_CHARGE);
-		// if (status < 0) {
-		// 	printk("Unable to fetch State of Charge\n");
-		// 	return;
-		// }
-
-		// status = sensor_channel_get(bq27,
-		// 			    SENSOR_CHAN_GAUGE_STATE_OF_CHARGE,
-		// 			    &state_of_charge);
-		// if (status < 0) {
-		// 	printk("Unable to get state of charge\n");
-		// 	return;
-		// }
-
-		// printk("State of charge: %d%%\n", state_of_charge.val1);
-
-		// status = sensor_sample_fetch_chan(bq27,
-		// 			SENSOR_CHAN_GAUGE_STATE_OF_HEALTH);
-		// if (status < 0) {
-		// 	printk("Failed to fetch State of Health\n");
-		// 	return;
-		// }
-
-		// status = sensor_channel_get(bq27,
-		// 			    SENSOR_CHAN_GAUGE_STATE_OF_HEALTH,
-		// 			    &state_of_health);
-		// if (status < 0) {
-		// 	printk("Unable to get state of charge\n");
-		// 	return;
-		// }
-
-		// printk("State of health: %d%%\n", state_of_health.val1);
-
-		// status = sensor_sample_fetch_chan(bq27,
-		// 			SENSOR_CHAN_GAUGE_AVG_POWER);
-		// if (status < 0) {
-		// 	printk("Unable to fetch Avg Power\n");
-		// 	return;
-		// }
-
-		// status = sensor_channel_get(bq27, SENSOR_CHAN_GAUGE_AVG_POWER,
-		// 			    &avg_power);
-		// if (status < 0) {
-		// 	printk("Unable to get avg power\n");
-		// 	return;
-		// }
-
-		// bq274xx_show_values("Avg Power in Watt", avg_power);
-
-		// status = sensor_sample_fetch_chan(bq27,
-		// 		SENSOR_CHAN_GAUGE_FULL_CHARGE_CAPACITY);
-		// if (status < 0) {
-		// 	printk("Failed to fetch Full Charge Capacity\n");
-		// 	return;
-		// }
-
-		// status = sensor_channel_get(bq27,
-		// 		SENSOR_CHAN_GAUGE_FULL_CHARGE_CAPACITY,
-		// 		&full_charge_capacity);
-		// if (status < 0) {
-		// 	printk("Unable to get full charge capacity\n");
-		// 	return;
-		// }
-
-		// printk("Full charge capacity: %d.%06dAh\n",
-		//        full_charge_capacity.val1, full_charge_capacity.val2);
-
-		// status = sensor_sample_fetch_chan(bq27,
-		// 		SENSOR_CHAN_GAUGE_REMAINING_CHARGE_CAPACITY);
-		// if (status < 0) {
-		// 	printk("Unable to fetch Remaining Charge Capacity\n");
-		// 	return;
-		// }
-
-		// status = sensor_channel_get(bq27,
-		// 		SENSOR_CHAN_GAUGE_REMAINING_CHARGE_CAPACITY,
-		// 		&remaining_charge_capacity);
-		// if (status < 0) {
-		// 	printk("Unable to get remaining charge capacity\n");
-		// 	return;
-		// }
-
-		// printk("Remaining charge capacity: %d.%06dAh\n",
-		//        remaining_charge_capacity.val1,
-		//        remaining_charge_capacity.val2);
-
-		// status = sensor_sample_fetch_chan(bq27, SENSOR_CHAN_GAUGE_TEMP);
-		// if (status < 0) {
-		// 	printk("Failed to fetch Gauge Temp\n");
-		// 	return;
-		// }
-
-		// status = sensor_channel_get(bq27, SENSOR_CHAN_GAUGE_TEMP,
-		// 			    &int_temp);
-		// if (status < 0) {
-		// 	printk("Unable to read internal temperature\n");
-		// 	return;
-		// }
-
-		// printk("Gauge Temperature: %d.%06d C\n", int_temp.val1,
-		//        int_temp.val2);
-    // ret = gpio_pin_toggle_dt(&led);
-    //  if (ret < 0) {
-    //     return;
-    // }
-    printk("degrees:%d\n",as5600_refresh(as));
 
     k_sleep(K_MSEC(1000));
   }
