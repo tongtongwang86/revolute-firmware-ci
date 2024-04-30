785b9dd5222192aaa56cb340a83c27e66fb9ec62
Tong tong <100080262+tongtongwang86@users.noreply.github.com>
2024-04-30 14:56:32 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,335 +1,361 @@
-/*
- * Copyright (c) 2016 Intel Corporation.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include <zephyr/drivers/uart.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/usb/usbd.h>
-#include <zephyr/logging/log.h>
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/usb/class/usb_hid.h>
 
-#include <zephyr/types.h>
-#include <stddef.h>
-#include <string.h>
-#include <errno.h>
-
-#include <zephyr/sys/byteorder.h>
+ 
 
 
-#include <soc.h>
+// #define HID_REVOLUTE_REPORT_DESC() {				\
+// 	HID_USAGE_PAGE(HID_USAGE_GEN_DESKTOP),			\
+// 	HID_USAGE(HID_USAGE_GEN_DESKTOP_MOUSE),			\
+// 	HID_COLLECTION(HID_COLLECTION_APPLICATION),		\
+// 		HID_COLLECTION(HID_COLLECTION_PHYSICAL),	\
+// 			HID_USAGE_PAGE(HID_USAGE_GEN_DESKTOP),	\
+// 			HID_USAGE(HID_USAGE_GEN_DESKTOP_WHEEL),	\
+// 			HID_LOGICAL_MIN8(-127),			\
+// 			HID_LOGICAL_MAX8(127),	
+// 			HID_REPORT_SIZE(8),			\
+// 			HID_REPORT_COUNT(1),			\
+// 			/* HID_INPUT (Data,Var,Rel) */		\
+// 			HID_INPUT(0x06),			\
+// 		HID_END_COLLECTION,				\
+// 	HID_END_COLLECTION,					\
+// }
 
-#include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/bluetooth/hci.h>
-#include <zephyr/bluetooth/conn.h>
-#include <zephyr/bluetooth/uuid.h>
-#include <zephyr/bluetooth/gatt.h>
+#define HID_REVLOTUE_REPORT_DESC(bcnt) {				\
+	HID_USAGE_PAGE(HID_USAGE_GEN_DESKTOP),			\
+	HID_USAGE(HID_USAGE_GEN_DESKTOP_MOUSE),			\
+	HID_COLLECTION(HID_COLLECTION_APPLICATION),		\
+		HID_USAGE(HID_USAGE_GEN_DESKTOP_POINTER),	\
+		HID_COLLECTION(HID_COLLECTION_PHYSICAL),	\
+			/* Bits used for button signalling */	\
+			HID_USAGE_PAGE(HID_USAGE_GEN_BUTTON),	\
+			HID_USAGE_MIN8(1),			\
+			HID_USAGE_MAX8(bcnt),			\
+			HID_LOGICAL_MIN8(0),			\
+			HID_LOGICAL_MAX8(1),			\
+			HID_REPORT_SIZE(1),			\
+			HID_REPORT_COUNT(bcnt),			\
+			/* HID_INPUT (Data,Var,Abs) */		\
+			HID_INPUT(0x02),			\
+			/* Unused bits */			\
+			HID_REPORT_SIZE(8 - bcnt),		\
+			HID_REPORT_COUNT(1),			\
+			/* HID_INPUT (Cnst,Ary,Abs) */		\
+			HID_INPUT(1),				\
+			/* X and Y axis, scroll */		\
+			HID_USAGE_PAGE(HID_USAGE_GEN_DESKTOP),	\
+			HID_USAGE(HID_USAGE_GEN_DESKTOP_X),	\
+			HID_USAGE(HID_USAGE_GEN_DESKTOP_Y),	\
+			HID_USAGE(HID_USAGE_GEN_DESKTOP_WHEEL),	\
+			HID_LOGICAL_MIN8(-127),			\
+			HID_LOGICAL_MAX8(127),			\
+			HID_REPORT_SIZE(8),			\
+			HID_REPORT_COUNT(3),			\
+			/* HID_INPUT (Data,Var,Rel) */		\
+			HID_INPUT(0x06),			\
+		HID_END_COLLECTION,				\
+	HID_END_COLLECTION,					\
+}
 
-#include "lbs.h"
+#define wheelResolution 1
+#define MOUSE_X_REPORT_POS	3
+// #define MOUSE_X_REPORT_POS	3
+uint8_t report[4] = { 0x00 };
+static volatile uint8_t status[4];
 
-#include <zephyr/settings/settings.h>
+#define KEY_1_CONFIGURE HID_KEY_S //clockwise key
+#define KEY_1_MODIFYER 0b00000010  // | RGUI| RALT| RSHIFT| RCONTROL | LGUI| LALT| LSHIFT| LCONTROL|
+
+#define KEY_2_CONFIGURE HID_KEY_S //counter clockwise key'
+#define KEY_2_MODIFYER 0b00000000  // | RGUI| RALT| RSHIFT| RCONTROL | LGUI| LALT| LSHIFT| LCONTROL|
+
+#define STACKSIZE 1024
+#define PRIORITY 1
+#define SLEEPTIME 500
+#define IDENT_OFFSET 1
+K_THREAD_STACK_DEFINE(threadA_stack_area, STACKSIZE);
+
+static struct k_thread threadA_data;
+
+static const struct device *hdev;
+
+static const uint8_t hid_rvl_report_desc[] = HID_REVLOTUE_REPORT_DESC(2);
+
+K_SEM_DEFINE(my_sem, 0, 10);
+static K_SEM_DEFINE(usb_sem, 1, 1);	/* starts off "available" */
+
+static void int_in_ready_cb(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+	
+	k_sem_give(&usb_sem);
+
+}
+
+
+static const struct hid_ops ops = {
+	.int_in_ready = int_in_ready_cb,
+};
+
+int as5600_refresh(const struct device *dev)
+{
+	int ret;
+    struct sensor_value rot_raw;
+    ret = sensor_sample_fetch_chan(dev,SENSOR_CHAN_ROTATION);
+	if (ret != 0){
+			printk("sample fetch error:,%d\n", ret);
+		}
+    sensor_channel_get(dev,SENSOR_CHAN_ROTATION, &rot_raw);
+	
+
+    return rot_raw.val1;
+}
+
+// int getNearestSnap (int num){
 
 
 
-#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
-#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
 
-#define RUN_STATUS_LED DK_LED1
-#define CON_STATUS_LED DK_LED2
-#define RUN_LED_BLINK_INTERVAL 1000
-
-#define USER_LED DK_LED3
-
-#define USER_BUTTON DK_BTN1_MSK
-
-static bool app_button_state;
+// }
 
 
 
-#define SW0_NODE	DT_ALIAS(sw0) 
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+void threadA(void *dummy1, void *dummy2, void *dummy3)
+{	
+	const struct device *const as = DEVICE_DT_GET(DT_INST(0,ams_as5600));
+
+	if (as == NULL || !device_is_ready(as)) {
+		printk("as5600 device tree not configured\n");
+		return;
+	}
+
+	int lastIdent = (as5600_refresh(as) - (as5600_refresh(as) % 12))/12 ;
+	
+	//int lastDeviation = as5600_refresh(as) % 12;
+	
+	
+	ARG_UNUSED(dummy1);
+	ARG_UNUSED(dummy2);
+	ARG_UNUSED(dummy3);
 
 
-#define LED0_NODE DT_ALIAS(led0)
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 
-LOG_MODULE_REGISTER(Rev,LOG_LEVEL_DBG);
+
+	while (1)
+	{
+		uint8_t state = status[MOUSE_X_REPORT_POS];
+		int degrees = as5600_refresh(as) ;
+		int usefulDegrees = as5600_refresh(as) ;
+		int lastDegree = as5600_refresh(as);
+		int deltadegrees = 0;
+		int lastDeltadegrees = 0;
+		int deltaDeltadegrees = 0;
+		int lastdeltaDeltadegrees = 0;
+		deltadegrees = (degrees-lastDegree);
+		deltaDeltadegrees = (deltadegrees-lastDeltadegrees);
+
+		//int deviation = degrees % 12;
+		// if (degrees-lastDegree < -200 ){
+		// 	if((degrees-lastDegree)-deltadegrees < -360){
+
+		// 		deltadegrees = 360*2 -(degrees-lastDegree);
+		// 		printk("aaaaa");
+
+		// 	}else{
+
+		// 		deltadegrees = 360 +(degrees-lastDegree);
+		// 	}
+			
+
+		// } else if (degrees-lastDegree > 200){
+
+		// 	if((degrees-lastDegree)-deltadegrees > 20){
+
+		// 		deltadegrees = 360*2 -(degrees-lastDegree);
+		// 		printk("eeeee");
+
+		// 	}else{
+
+		// 		deltadegrees = 360 -(degrees-lastDegree);
+		// 	}
+
+		// } else {
+
+		// 	deltadegrees = (degrees-lastDegree);
+
+		// }
+
+		if (deltaDeltadegrees < -200){
+			usefulDegrees = lastDeltadegrees ;
+			deltaDeltadegrees = lastdeltaDeltadegrees;
+
+		} else if(deltaDeltadegrees > 200) {
+			usefulDegrees = lastDeltadegrees ;
+			deltaDeltadegrees = lastdeltaDeltadegrees;
+
+		}else{
+			usefulDegrees = (degrees-lastDegree) ;
+
+		}
+		// lastDegree = degrees;
+		
+		//printk("%d\n", (degrees - (degrees % 12))/12);
+		// printk("%d\n", usefulDegrees);
+if (degrees != lastDegree){
+		if (lastIdent != (((degrees+6+IDENT_OFFSET)) - ((degrees+6+IDENT_OFFSET) % 12))/12 && (((degrees+6+IDENT_OFFSET)) - ((degrees+6+IDENT_OFFSET) % 12))/12!= 30) {
+
+		// if (degrees != lastDegree){
+
+
+		
+		uint8_t rep[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+		
+		if (deltaDeltadegrees > 0){
+			// state += (deltaDeltadegrees);
+			state += 1U;
+
+		}else{
+			// state += (deltaDeltadegrees);
+			state -= 1U;
+		}
+
+		status[MOUSE_X_REPORT_POS] = state;
+		report[MOUSE_X_REPORT_POS] = status[MOUSE_X_REPORT_POS];
+		status[MOUSE_X_REPORT_POS] = 0U;
+
+	
+
+		k_sem_take(&usb_sem, K_FOREVER);
+		int ret = hid_int_ep_write(hdev,report,sizeof(report), NULL);
+		// printk("snap:%d\n",degrees);
+		printk("degrees:%d\n",as5600_refresh(as));
+		
+	
+		k_sem_give(&my_sem);
+		}
+			lastIdent = ((degrees+6+IDENT_OFFSET) - ((degrees+6+IDENT_OFFSET) % 12))/12;
+			lastDegree = degrees;
+			lastDeltadegrees = deltadegrees;
+			lastdeltaDeltadegrees = deltaDeltadegrees;
+			//lastDeviation = deviation;
+		}
+
+
+	}
+
+}
+
+
+
+
 
 
 BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
-             "Console device is not ACM CDC UART device");
-
-
-static const struct bt_data ad[] = {
-	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
-  BT_DATA_BYTES(BT_DATA_GAP_APPEARANCE, 0xC1, 0x03),
-  BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-  BT_DATA_BYTES(BT_DATA_UUID16_SOME, 0x12, 0x18, /* HID Service */
-                  0x0f, 0x18                       /* Battery Service */
-                  ),
-
-};
-
-
-// static const struct bt_data zmk_ble_ad[] = {
-//     BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
-//     BT_DATA_BYTES(BT_DATA_GAP_APPEARANCE, 0xC1, 0x03),
-//     BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-//     BT_DATA_BYTES(BT_DATA_UUID16_SOME, 0x12, 0x18, /* HID Service */
-//                   0x0f, 0x18                       /* Battery Service */
-//                   ),
-// };
-
-
-static const struct bt_data sd[] = {
-	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_LBS_VAL),
-};
-
-static void connected(struct bt_conn *conn, uint8_t err)
-{
-	if (err) {
-		printk("Connection failed (err %u)\n", err);
-		return;
-	}
-
-	printk("Connected\n");
-
-	// dk_set_led_on(CON_STATUS_LED);
-  gpio_pin_toggle_dt(&led);
-  // gpio_pin_set_dt(&led, 1);
-}
-
-static void disconnected(struct bt_conn *conn, uint8_t reason)
-{
-	printk("Disconnected (reason %u)\n", reason);
-
-	// gpio_pin_set_dt(&led, 0);
-  gpio_pin_toggle_dt(&led);
-}
-
-static void on_security_changed(struct bt_conn *conn, bt_security_t level,
-			     enum bt_security_err err)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	if (!err) {
-		LOG_INF("Security changed: %s level %u\n", addr, level);
-	} else {
-		LOG_INF("Security failed: %s level %u err %d\n", addr, level,
-			err);
-	}
-}
-
-
-BT_CONN_CB_DEFINE(conn_callbacks) = {
-	.connected = connected,
-	.disconnected = disconnected,
-// #ifdef CONFIG_BT_LBS_SECURITY_ENABLED
-	.security_changed = on_security_changed,
-// #endif
-};
-
-
-#if defined(CONFIG_BT_LBS_SECURITY_ENABLED)
-static void auth_passkey_display(struct bt_conn *conn, unsigned int passkey)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	printk("Passkey for %s: %06u\n", addr, passkey);
-}
-
-static void auth_cancel(struct bt_conn *conn)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	printk("Pairing cancelled: %s\n", addr);
-}
-
-static void pairing_complete(struct bt_conn *conn, bool bonded)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	printk("Pairing completed: %s, bonded: %d\n", addr, bonded);
-}
-
-static void pairing_failed(struct bt_conn *conn, enum bt_security_err reason)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	printk("Pairing failed conn: %s, reason %d\n", addr, reason);
-}
-
-static struct bt_conn_auth_cb conn_auth_callbacks = {
-	.passkey_display = auth_passkey_display,
-	.cancel = auth_cancel,
-};
-
-static struct bt_conn_auth_info_cb conn_auth_info_callbacks = { .pairing_complete =
-									pairing_complete,
-								.pairing_failed = pairing_failed };
-#else
-static struct bt_conn_auth_cb conn_auth_callbacks;
-static struct bt_conn_auth_info_cb conn_auth_info_callbacks;
-#endif
-
-static void app_led_cb(bool led_state)
-{
-	// gpio_pin_set_dt(&led, led_state);
-  gpio_pin_toggle_dt(&led);
-}
-
-static bool app_button_cb(void)
-{
- LOG_INF("POLLED FOR BUTTON STATE");
-	return app_button_state;
- 
-
-}
-
-static struct bt_lbs_cb lbs_callbacs = {
-	.led_cb = app_led_cb,
-	.button_cb = app_button_cb,
-};
-
-
-void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
-{
-    // gpio_pin_toggle_dt(&led);
-    gpio_pin_toggle_dt(&led);
-
-    bt_lbs_send_button_state(app_button_state);
-    app_button_state = !app_button_state;
-    LOG_INF("toggled led");
-}
-
-static struct gpio_callback button_cb_data;
-
-int main(void) {
-
-  const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-  uint32_t dtr = 0;
+	     "Console device is not ACM CDC UART device");
 
 #if defined(CONFIG_USB_DEVICE_STACK_NEXT)
-  if (enable_usb_device_next()) {
-    return 0;
-  }
-#else
-  if (usb_enable(NULL)) {
-    return 0;
-  }
-#endif
+USBD_CONFIGURATION_DEFINE(config_1,
+			  USB_SCD_SELF_POWERED,
+			  200);
 
+USBD_DESC_LANG_DEFINE(sample_lang);
+USBD_DESC_MANUFACTURER_DEFINE(sample_mfr, "ZEPHYR");
+USBD_DESC_PRODUCT_DEFINE(sample_product, "Zephyr USBD ACM console");
+USBD_DESC_SERIAL_NUMBER_DEFINE(sample_sn, "0123456789ABCDEF");
 
-  /* Poll if the DTR flag was set */
-  while (!dtr) {
-    uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-    /* Give CPU resources to low priority threads. */
-    k_sleep(K_MSEC(100));
-  }
+USBD_DEVICE_DEFINE(sample_usbd,
+		   DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0)),
+		   0x2fe3, 0x0001);
 
+static int enable_usb_device_next(void)
+{
 
+	usbd_add_descriptor(&sample_usbd, &sample_lang);
+	usbd_add_descriptor(&sample_usbd, &sample_mfr);
+	usbd_add_descriptor(&sample_usbd, &sample_product);
+	usbd_add_descriptor(&sample_usbd, &sample_sn);
+	usbd_add_configuration(&sample_usbd, &config_1);
+	usbd_register_class(&sample_usbd, "cdc_acm_0", 1);
+	usbd_init(&sample_usbd);
+	usbd_enable(&sample_usbd);
+	
+	return 0;
+}
 
-  if (!gpio_is_ready_dt(&led)) {
+#endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK_NEXT) */
+
+int main(void)
+{
+	const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+	uint32_t dtr = 0;
+
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
+	if (enable_usb_device_next()) {
 		return 0;
 	}
+#else
+	if (usb_enable(NULL)) {
+		return 0;
+	}
+#endif
 
-  if (!device_is_ready(button.port)) {
-	return -1;
+	
+	k_thread_create(&threadA_data, threadA_stack_area,
+			K_THREAD_STACK_SIZEOF(threadA_stack_area),
+			threadA, NULL, NULL, NULL,
+			PRIORITY, 0, K_FOREVER);
+	k_thread_name_set(&threadA_data, "thread_a");
+
+	k_thread_start(&threadA_data);
+
+	
+
+
+while (1)
+	{	
+
+		if (k_sem_take(&my_sem, K_MSEC(50)) != 0) {
+  
+    } else {
+		
+		
+
+		// uint8_t rep[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+	
+		// k_sem_take(&usb_sem, K_FOREVER);
+		// int ret = hid_int_ep_write(hdev,rep,sizeof(rep), NULL);
+
+		
+		
+
+    }
+	
+	}
+
+	
 }
 
-int ret;
-int err;
 
-ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-if (ret < 0) {
-    return;
-}
-
-ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-	if (ret < 0) {
-		return -1;
+static int composite_pre_init(void)
+{
+	hdev = device_get_binding("HID_0");
+	if (hdev == NULL) {
+		printk("Cannot get USB HID Device");
+		return -ENODEV;
 	}
 
 
-  ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	usb_hid_register_device(hdev, hid_rvl_report_desc,sizeof(hid_rvl_report_desc), &ops);
 
-if (ret < 0) {
-  LOG_ERR("set pin as interrupt failed");
-	return -1;
+	return usb_hid_init(hdev);
 }
 
 
-
-
-    gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin)); 	
-	gpio_add_callback(button.port, &button_cb_data);
-
-
-if (IS_ENABLED(CONFIG_BT_LBS_SECURITY_ENABLED)) {
-		err = bt_conn_auth_cb_register(&conn_auth_callbacks);
-		if (err) {
-			printk("Failed to register authorization callbacks.\n");
-			return;
-		}
-
-		err = bt_conn_auth_info_cb_register(&conn_auth_info_callbacks);
-		if (err) {
-			printk("Failed to register authorization info callbacks.\n");
-			return;
-		}
-	}
-
-	err = bt_enable(NULL);
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
-		return;
-	}
-
-	printk("Bluetooth initialized\n");
-
-  if (IS_ENABLED(CONFIG_SETTINGS)) {
-		settings_load();
-	}
-
-  	err = bt_lbs_init(&lbs_callbacs);
-	if (err) {
-		printk("Failed to init LBS (err:%d)\n", err);
-		return;
-	}
-
-  err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
-	if (err) {
-		printk("Advertising failed to start (err %d)\n", err);
-		return;
-	}
-  printk("Advertising successfully started\n");
-
-  while (1) {
-    // bool val = gpio_pin_get_dt(&button);
-    // gpio_pin_set_dt(&led,val);
-    // printk("Hello World! %s\n", CONFIG_ARCH);
-    // LOG_INF("running");
-    // LOG_DBG("A log message in debug level");
-    // LOG_WRN("A log message in warning level!");
-    // LOG_ERR("A log message in Error level!");
-    // ret = gpio_pin_toggle_dt(&led);
-    //  if (ret < 0) {
-    //     return;
-    // }
-    // k_sleep(K_MSEC(1000));
-  }
-}
+SYS_INIT(composite_pre_init, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
