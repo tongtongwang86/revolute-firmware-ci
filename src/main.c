04de5254999db49a39bfeb6f7f062142fb166251
Tong tong <100080262+tongtongwang86@users.noreply.github.com>
2024-05-07 14:28:04 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,335 +1,413 @@
 /*
- * Copyright (c) 2016 Intel Corporation.
+ * Copyright (c) 2016 Nordic Semiconductor ASA
+ * Copyright (c) 2015-2016 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/drivers/uart.h>
-#include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/usb/usb_device.h>
-#include <zephyr/usb/usbd.h>
-#include <zephyr/logging/log.h>
-#include <zephyr/drivers/gpio.h>
-
-#include <zephyr/types.h>
-#include <stddef.h>
-#include <string.h>
 #include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 
+#include <zephyr/kernel.h>
+#include <zephyr/arch/cpu.h>
 #include <zephyr/sys/byteorder.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/util.h>
 
+#include <zephyr/device.h>
+#include <zephyr/init.h>
+#include <zephyr/drivers/uart.h>
 
-#include <soc.h>
+#include <zephyr/usb/usb_device.h>
 
+#include <zephyr/net/buf.h>
 #include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/l2cap.h>
 #include <zephyr/bluetooth/hci.h>
-#include <zephyr/bluetooth/conn.h>
-#include <zephyr/bluetooth/uuid.h>
-#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/buf.h>
+#include <zephyr/bluetooth/hci_raw.h>
 
-#include "lbs.h"
+#define LOG_MODULE_NAME hci_uart
+LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
-#include <zephyr/settings/settings.h>
+static const struct device *const hci_uart_dev =
+	DEVICE_DT_GET(DT_CHOSEN(zephyr_bt_c2h_uart));
+static K_THREAD_STACK_DEFINE(tx_thread_stack, CONFIG_BT_HCI_TX_STACK_SIZE);
+static struct k_thread tx_thread_data;
+static K_FIFO_DEFINE(tx_queue);
 
+/* RX in terms of bluetooth communication */
+static K_FIFO_DEFINE(uart_tx_queue);
 
+#define H4_CMD 0x01
+#define H4_ACL 0x02
+#define H4_SCO 0x03
+#define H4_EVT 0x04
+#define H4_ISO 0x05
 
-#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
-#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+/* Receiver states. */
+#define ST_IDLE 0	/* Waiting for packet type. */
+#define ST_HDR 1	/* Receiving packet header. */
+#define ST_PAYLOAD 2	/* Receiving packet payload. */
+#define ST_DISCARD 3	/* Dropping packet. */
 
-#define RUN_STATUS_LED DK_LED1
-#define CON_STATUS_LED DK_LED2
-#define RUN_LED_BLINK_INTERVAL 1000
+/* Length of a discard/flush buffer.
+ * This is sized to align with a BLE HCI packet:
+ * 1 byte H:4 header + 32 bytes ACL/event data
+ * Bigger values might overflow the stack since this is declared as a local
+ * variable, smaller ones will force the caller to call into discard more
+ * often.
+ */
+#define H4_DISCARD_LEN 33
 
-#define USER_LED DK_LED3
-
-#define USER_BUTTON DK_BTN1_MSK
-
-static bool app_button_state;
-
-
-
-#define SW0_NODE	DT_ALIAS(sw0) 
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
-
-
-#define LED0_NODE DT_ALIAS(led0)
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-
-
-LOG_MODULE_REGISTER(Rev,LOG_LEVEL_DBG);
-
-
-BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
-             "Console device is not ACM CDC UART device");
-
-
-static const struct bt_data ad[] = {
-	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
-  BT_DATA_BYTES(BT_DATA_GAP_APPEARANCE, 0xC1, 0x03),
-  BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-  BT_DATA_BYTES(BT_DATA_UUID16_SOME, 0x12, 0x18, /* HID Service */
-                  0x0f, 0x18                       /* Battery Service */
-                  ),
-
-};
-
-
-// static const struct bt_data zmk_ble_ad[] = {
-//     BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
-//     BT_DATA_BYTES(BT_DATA_GAP_APPEARANCE, 0xC1, 0x03),
-//     BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-//     BT_DATA_BYTES(BT_DATA_UUID16_SOME, 0x12, 0x18, /* HID Service */
-//                   0x0f, 0x18                       /* Battery Service */
-//                   ),
-// };
-
-
-static const struct bt_data sd[] = {
-	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_LBS_VAL),
-};
-
-static void connected(struct bt_conn *conn, uint8_t err)
+static int h4_read(const struct device *uart, uint8_t *buf, size_t len)
 {
-	if (err) {
-		printk("Connection failed (err %u)\n", err);
-		return;
-	}
+	int rx = uart_fifo_read(uart, buf, len);
 
-	printk("Connected\n");
+	LOG_DBG("read %d req %d", rx, len);
 
-	// dk_set_led_on(CON_STATUS_LED);
-  gpio_pin_toggle_dt(&led);
-  // gpio_pin_set_dt(&led, 1);
+	return rx;
 }
 
-static void disconnected(struct bt_conn *conn, uint8_t reason)
+static bool valid_type(uint8_t type)
 {
-	printk("Disconnected (reason %u)\n", reason);
-
-	// gpio_pin_set_dt(&led, 0);
-  gpio_pin_toggle_dt(&led);
+	return (type == H4_CMD) | (type == H4_ACL) | (type == H4_ISO);
 }
 
-static void on_security_changed(struct bt_conn *conn, bt_security_t level,
-			     enum bt_security_err err)
+/* Function expects that type is validated and only CMD, ISO or ACL will be used. */
+static uint32_t get_len(const uint8_t *hdr_buf, uint8_t type)
 {
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	if (!err) {
-		LOG_INF("Security changed: %s level %u\n", addr, level);
-	} else {
-		LOG_INF("Security failed: %s level %u err %d\n", addr, level,
-			err);
-	}
-}
-
-
-BT_CONN_CB_DEFINE(conn_callbacks) = {
-	.connected = connected,
-	.disconnected = disconnected,
-// #ifdef CONFIG_BT_LBS_SECURITY_ENABLED
-	.security_changed = on_security_changed,
-// #endif
-};
-
-
-#if defined(CONFIG_BT_LBS_SECURITY_ENABLED)
-static void auth_passkey_display(struct bt_conn *conn, unsigned int passkey)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	printk("Passkey for %s: %06u\n", addr, passkey);
-}
-
-static void auth_cancel(struct bt_conn *conn)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	printk("Pairing cancelled: %s\n", addr);
-}
-
-static void pairing_complete(struct bt_conn *conn, bool bonded)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	printk("Pairing completed: %s, bonded: %d\n", addr, bonded);
-}
-
-static void pairing_failed(struct bt_conn *conn, enum bt_security_err reason)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	printk("Pairing failed conn: %s, reason %d\n", addr, reason);
-}
-
-static struct bt_conn_auth_cb conn_auth_callbacks = {
-	.passkey_display = auth_passkey_display,
-	.cancel = auth_cancel,
-};
-
-static struct bt_conn_auth_info_cb conn_auth_info_callbacks = { .pairing_complete =
-									pairing_complete,
-								.pairing_failed = pairing_failed };
-#else
-static struct bt_conn_auth_cb conn_auth_callbacks;
-static struct bt_conn_auth_info_cb conn_auth_info_callbacks;
-#endif
-
-static void app_led_cb(bool led_state)
-{
-	// gpio_pin_set_dt(&led, led_state);
-  gpio_pin_toggle_dt(&led);
-}
-
-static bool app_button_cb(void)
-{
- LOG_INF("POLLED FOR BUTTON STATE");
-	return app_button_state;
- 
-
-}
-
-static struct bt_lbs_cb lbs_callbacs = {
-	.led_cb = app_led_cb,
-	.button_cb = app_button_cb,
-};
-
-
-void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
-{
-    // gpio_pin_toggle_dt(&led);
-    gpio_pin_toggle_dt(&led);
-
-    bt_lbs_send_button_state(app_button_state);
-    app_button_state = !app_button_state;
-    LOG_INF("toggled led");
-}
-
-static struct gpio_callback button_cb_data;
-
-int main(void) {
-
-  const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-  uint32_t dtr = 0;
-
-#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
-  if (enable_usb_device_next()) {
-    return 0;
-  }
-#else
-  if (usb_enable(NULL)) {
-    return 0;
-  }
-#endif
-
-
-  /* Poll if the DTR flag was set */
-  while (!dtr) {
-    uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-    /* Give CPU resources to low priority threads. */
-    k_sleep(K_MSEC(100));
-  }
-
-
-
-  if (!gpio_is_ready_dt(&led)) {
+	switch (type) {
+	case H4_CMD:
+		return ((const struct bt_hci_cmd_hdr *)hdr_buf)->param_len;
+	case H4_ISO:
+		return bt_iso_hdr_len(
+			sys_le16_to_cpu(((const struct bt_hci_iso_hdr *)hdr_buf)->len));
+	case H4_ACL:
+		return sys_le16_to_cpu(((const struct bt_hci_acl_hdr *)hdr_buf)->len);
+	default:
+		LOG_ERR("Invalid type: %u", type);
 		return 0;
 	}
-
-  if (!device_is_ready(button.port)) {
-	return -1;
 }
 
-int ret;
-int err;
-
-ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-if (ret < 0) {
-    return;
-}
-
-ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-	if (ret < 0) {
-		return -1;
+/* Function expects that type is validated and only CMD, ISO or ACL will be used. */
+static int hdr_len(uint8_t type)
+{
+	switch (type) {
+	case H4_CMD:
+		return sizeof(struct bt_hci_cmd_hdr);
+	case H4_ISO:
+		return sizeof(struct bt_hci_iso_hdr);
+	case H4_ACL:
+		return sizeof(struct bt_hci_acl_hdr);
+	default:
+		LOG_ERR("Invalid type: %u", type);
+		return 0;
 	}
-
-
-  ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
-
-if (ret < 0) {
-  LOG_ERR("set pin as interrupt failed");
-	return -1;
 }
 
+static void rx_isr(void)
+{
+	static struct net_buf *buf;
+	static int remaining;
+	static uint8_t state;
+	static uint8_t type;
+	static uint8_t hdr_buf[MAX(sizeof(struct bt_hci_cmd_hdr),
+			sizeof(struct bt_hci_acl_hdr))];
+	int read;
 
+	do {
+		switch (state) {
+		case ST_IDLE:
+			/* Get packet type */
+			read = h4_read(hci_uart_dev, &type, sizeof(type));
+			/* since we read in loop until no data is in the fifo,
+			 * it is possible that read = 0.
+			 */
+			if (read) {
+				if (valid_type(type)) {
+					/* Get expected header size and switch
+					 * to receiving header.
+					 */
+					remaining = hdr_len(type);
+					state = ST_HDR;
+				} else {
+					LOG_WRN("Unknown header %d", type);
+				}
+			}
+			break;
+		case ST_HDR:
+			read = h4_read(hci_uart_dev,
+				       &hdr_buf[hdr_len(type) - remaining],
+				       remaining);
+			remaining -= read;
+			if (remaining == 0) {
+				/* Header received. Allocate buffer and get
+				 * payload length. If allocation fails leave
+				 * interrupt. On failed allocation state machine
+				 * is reset.
+				 */
+				buf = bt_buf_get_tx(BT_BUF_H4, K_NO_WAIT,
+						    &type, sizeof(type));
+				if (!buf) {
+					LOG_ERR("No available command buffers!");
+					state = ST_IDLE;
+					return;
+				}
 
+				remaining = get_len(hdr_buf, type);
 
-    gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin)); 	
-	gpio_add_callback(button.port, &button_cb_data);
+				net_buf_add_mem(buf, hdr_buf, hdr_len(type));
+				if (remaining > net_buf_tailroom(buf)) {
+					LOG_ERR("Not enough space in buffer");
+					net_buf_unref(buf);
+					state = ST_DISCARD;
+				} else {
+					state = ST_PAYLOAD;
+				}
 
+			}
+			break;
+		case ST_PAYLOAD:
+			read = h4_read(hci_uart_dev, net_buf_tail(buf),
+				       remaining);
+			buf->len += read;
+			remaining -= read;
+			if (remaining == 0) {
+				/* Packet received */
+				LOG_DBG("putting RX packet in queue.");
+				net_buf_put(&tx_queue, buf);
+				state = ST_IDLE;
+			}
+			break;
+		case ST_DISCARD:
+		{
+			uint8_t discard[H4_DISCARD_LEN];
+			size_t to_read = MIN(remaining, sizeof(discard));
 
-if (IS_ENABLED(CONFIG_BT_LBS_SECURITY_ENABLED)) {
-		err = bt_conn_auth_cb_register(&conn_auth_callbacks);
-		if (err) {
-			printk("Failed to register authorization callbacks.\n");
+			read = h4_read(hci_uart_dev, discard, to_read);
+			remaining -= read;
+			if (remaining == 0) {
+				state = ST_IDLE;
+			}
+
+			break;
+
+		}
+		default:
+			read = 0;
+			__ASSERT_NO_MSG(0);
+			break;
+
+		}
+	} while (read);
+}
+
+static void tx_isr(void)
+{
+	static struct net_buf *buf;
+	int len;
+
+	if (!buf) {
+		buf = net_buf_get(&uart_tx_queue, K_NO_WAIT);
+		if (!buf) {
+			uart_irq_tx_disable(hci_uart_dev);
 			return;
 		}
+	}
 
-		err = bt_conn_auth_info_cb_register(&conn_auth_info_callbacks);
+	len = uart_fifo_fill(hci_uart_dev, buf->data, buf->len);
+	net_buf_pull(buf, len);
+	if (!buf->len) {
+		net_buf_unref(buf);
+		buf = NULL;
+	}
+}
+
+static void bt_uart_isr(const struct device *unused, void *user_data)
+{
+	ARG_UNUSED(unused);
+	ARG_UNUSED(user_data);
+
+	if (!(uart_irq_rx_ready(hci_uart_dev) ||
+	      uart_irq_tx_ready(hci_uart_dev))) {
+		LOG_DBG("spurious interrupt");
+	}
+
+	if (uart_irq_tx_ready(hci_uart_dev)) {
+		tx_isr();
+	}
+
+	if (uart_irq_rx_ready(hci_uart_dev)) {
+		rx_isr();
+	}
+}
+
+static void tx_thread(void *p1, void *p2, void *p3)
+{
+	while (1) {
+		struct net_buf *buf;
+		int err;
+
+		/* Wait until a buffer is available */
+		buf = net_buf_get(&tx_queue, K_FOREVER);
+		/* Pass buffer to the stack */
+		err = bt_send(buf);
 		if (err) {
-			printk("Failed to register authorization info callbacks.\n");
-			return;
+			LOG_ERR("Unable to send (err %d)", err);
+			net_buf_unref(buf);
+		}
+
+		/* Give other threads a chance to run if tx_queue keeps getting
+		 * new data all the time.
+		 */
+		k_yield();
+	}
+}
+
+static int h4_send(struct net_buf *buf)
+{
+	LOG_DBG("buf %p type %u len %u", buf, bt_buf_get_type(buf),
+		    buf->len);
+
+	net_buf_put(&uart_tx_queue, buf);
+	uart_irq_tx_enable(hci_uart_dev);
+
+	return 0;
+}
+
+#if defined(CONFIG_BT_CTLR_ASSERT_HANDLER)
+void bt_ctlr_assert_handle(char *file, uint32_t line)
+{
+	uint32_t len = 0U, pos = 0U;
+
+	/* Disable interrupts, this is unrecoverable */
+	(void)irq_lock();
+
+	uart_irq_rx_disable(hci_uart_dev);
+	uart_irq_tx_disable(hci_uart_dev);
+
+	if (file) {
+		while (file[len] != '\0') {
+			if (file[len] == '/') {
+				pos = len + 1;
+			}
+			len++;
+		}
+		file += pos;
+		len -= pos;
+	}
+
+	uart_poll_out(hci_uart_dev, H4_EVT);
+	/* Vendor-Specific debug event */
+	uart_poll_out(hci_uart_dev, 0xff);
+	/* 0xAA + strlen + \0 + 32-bit line number */
+	uart_poll_out(hci_uart_dev, 1 + len + 1 + 4);
+	uart_poll_out(hci_uart_dev, 0xAA);
+
+	if (len) {
+		while (*file != '\0') {
+			uart_poll_out(hci_uart_dev, *file);
+			file++;
+		}
+		uart_poll_out(hci_uart_dev, 0x00);
+	}
+
+	uart_poll_out(hci_uart_dev, line >> 0 & 0xff);
+	uart_poll_out(hci_uart_dev, line >> 8 & 0xff);
+	uart_poll_out(hci_uart_dev, line >> 16 & 0xff);
+	uart_poll_out(hci_uart_dev, line >> 24 & 0xff);
+
+	while (1) {
+	}
+}
+#endif /* CONFIG_BT_CTLR_ASSERT_HANDLER */
+
+static int hci_uart_init(void)
+{
+	LOG_DBG("");
+
+	if (IS_ENABLED(CONFIG_USB_CDC_ACM)) {
+		if (usb_enable(NULL)) {
+			LOG_ERR("Failed to enable USB");
+			return -EINVAL;
 		}
 	}
 
-	err = bt_enable(NULL);
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
-		return;
+	if (!device_is_ready(hci_uart_dev)) {
+		LOG_ERR("HCI UART %s is not ready", hci_uart_dev->name);
+		return -EINVAL;
 	}
 
-	printk("Bluetooth initialized\n");
+	uart_irq_rx_disable(hci_uart_dev);
+	uart_irq_tx_disable(hci_uart_dev);
 
-  if (IS_ENABLED(CONFIG_SETTINGS)) {
-		settings_load();
+	uart_irq_callback_set(hci_uart_dev, bt_uart_isr);
+
+	uart_irq_rx_enable(hci_uart_dev);
+
+	return 0;
+}
+
+SYS_INIT(hci_uart_init, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
+
+int main(void)
+{
+	/* incoming events and data from the controller */
+	static K_FIFO_DEFINE(rx_queue);
+	int err;
+
+	LOG_DBG("Start");
+	__ASSERT(hci_uart_dev, "UART device is NULL");
+
+	/* Enable the raw interface, this will in turn open the HCI driver */
+	bt_enable_raw(&rx_queue);
+
+	if (IS_ENABLED(CONFIG_BT_WAIT_NOP)) {
+		/* Issue a Command Complete with NOP */
+		int i;
+
+		const struct {
+			const uint8_t h4;
+			const struct bt_hci_evt_hdr hdr;
+			const struct bt_hci_evt_cmd_complete cc;
+		} __packed cc_evt = {
+			.h4 = H4_EVT,
+			.hdr = {
+				.evt = BT_HCI_EVT_CMD_COMPLETE,
+				.len = sizeof(struct bt_hci_evt_cmd_complete),
+			},
+			.cc = {
+				.ncmd = 1,
+				.opcode = sys_cpu_to_le16(BT_OP_NOP),
+			},
+		};
+
+		for (i = 0; i < sizeof(cc_evt); i++) {
+			uart_poll_out(hci_uart_dev,
+				      *(((const uint8_t *)&cc_evt)+i));
+		}
 	}
 
-  	err = bt_lbs_init(&lbs_callbacs);
-	if (err) {
-		printk("Failed to init LBS (err:%d)\n", err);
-		return;
-	}
+	/* Spawn the TX thread and start feeding commands and data to the
+	 * controller
+	 */
+	k_thread_create(&tx_thread_data, tx_thread_stack,
+			K_THREAD_STACK_SIZEOF(tx_thread_stack), tx_thread,
+			NULL, NULL, NULL, K_PRIO_COOP(7), 0, K_NO_WAIT);
+	k_thread_name_set(&tx_thread_data, "HCI uart TX");
 
-  err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
-	if (err) {
-		printk("Advertising failed to start (err %d)\n", err);
-		return;
-	}
-  printk("Advertising successfully started\n");
+	while (1) {
+		struct net_buf *buf;
 
-  while (1) {
-    // bool val = gpio_pin_get_dt(&button);
-    // gpio_pin_set_dt(&led,val);
-    // printk("Hello World! %s\n", CONFIG_ARCH);
-    // LOG_INF("running");
-    // LOG_DBG("A log message in debug level");
-    // LOG_WRN("A log message in warning level!");
-    // LOG_ERR("A log message in Error level!");
-    // ret = gpio_pin_toggle_dt(&led);
-    //  if (ret < 0) {
-    //     return;
-    // }
-    // k_sleep(K_MSEC(1000));
-  }
+		buf = net_buf_get(&rx_queue, K_FOREVER);
+		err = h4_send(buf);
+		if (err) {
+			LOG_ERR("Failed to send");
+		}
+	}
+	return 0;
 }
