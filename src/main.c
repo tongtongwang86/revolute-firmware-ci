c76508e97d193cdb0b9910af727c0c185fb64466
Tong tong <100080262+tongtongwang86@users.noreply.github.com>
2024-05-26 02:48:29 +0800

    adding console

--- src/main.c
+++ src/main.c
@@ -6,6 +6,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+
+
+
+
+
 #include <zephyr/types.h>
 #include <stddef.h>
 #include <string.h>
@@ -23,6 +28,24 @@
 #include <zephyr/bluetooth/gatt.h>
 
 #include "hog.h"
+
+// logging stuff
+#include <zephyr/logging/log.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/usbd.h>
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+
+LOG_MODULE_REGISTER(Rev,LOG_LEVEL_DBG);
+BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
+             "Console device is not ACM CDC UART device");
+
+
+
+
+
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -141,6 +164,30 @@ static struct bt_conn_auth_cb auth_cb_display = {
 
 int main(void)
 {
+
+	const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+  uint32_t dtr = 0;
+
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
+  if (enable_usb_device_next()) {
+    return 0;
+  }
+#else
+  if (usb_enable(NULL)) {
+    return 0;
+  }
+#endif
+
+
+  /* Poll if the DTR flag was set */
+  while (!dtr) {
+    uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+    /* Give CPU resources to low priority threads. */
+    k_sleep(K_MSEC(100));
+  }
+
+
+	
 	int err;
 
 	err = bt_enable(bt_ready);
