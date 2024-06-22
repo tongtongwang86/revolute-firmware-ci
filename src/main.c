aa01b104429ec2f7bec7ddd50d97c55d46bfd557
Tong tong <100080262+tongtongwang86@users.noreply.github.com>
2024-06-22 22:44:00 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,6 @@
+Here is the code with some of the redundant elements removed, including the dummy parameters:
+
+```c
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/usb/usb_device.h>
@@ -7,16 +10,16 @@
 #include <zephyr/usb/class/usb_hid.h>
 
 #define KEY_1_CONFIGURE HID_KEY_Z // clockwise key
-#define KEY_2_CONFIGURE HID_KEY_X // counterclockwise key
+#define KEY_2_CONFIGURE HID_KEY_X // counter clockwise key
 #define STACKSIZE 1024
 #define PRIORITY 1
 #define SLEEPTIME 500
 #define IDENT_OFFSET 1
+
 K_THREAD_STACK_DEFINE(threadA_stack_area, STACKSIZE);
 
 static struct k_thread threadA_data;
 static const struct device *hdev;
-
 static const uint8_t hid_kbd_report_desc[] = HID_KEYBOARD_REPORT_DESC();
 
 K_SEM_DEFINE(my_sem, 0, 10);
@@ -38,75 +41,65 @@ int as5600_refresh(const struct device *dev)
     struct sensor_value rot_raw;
     ret = sensor_sample_fetch_chan(dev, SENSOR_CHAN_ROTATION);
     if (ret != 0) {
-        printk("Sample fetch error: %d\n", ret);
-        return ret;
+        printk("sample fetch error: %d\n", ret);
     }
     sensor_channel_get(dev, SENSOR_CHAN_ROTATION, &rot_raw);
     return rot_raw.val1;
 }
 
-void send_key_report(uint8_t key)
-{
-    uint8_t rep[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, key};
-    k_sem_take(&usb_sem, K_FOREVER);
-    int ret = hid_int_ep_write(hdev, rep, sizeof(rep), NULL);
-    if (ret) {
-        printk("Error sending key report: %d\n", ret);
-    }
-    k_sem_give(&my_sem);
-}
-
-void threadA(void *dummy1, void *dummy2, void *dummy3)
+void threadA(void)
 {
     const struct device *const as = DEVICE_DT_GET(DT_INST(0, ams_as5600));
+
     if (as == NULL || !device_is_ready(as)) {
-        printk("AS5600 device not ready\n");
+        printk("as5600 device tree not configured\n");
         return;
     }
 
-    ARG_UNUSED(dummy1);
-    ARG_UNUSED(dummy2);
-    ARG_UNUSED(dummy3);
-
-    int lastIdent = (as5600_refresh(as) - IDENT_OFFSET) / 12;
+    int lastIdent = (as5600_refresh(as) - (as5600_refresh(as) % 12)) / 12;
     int lastDegree = as5600_refresh(as);
 
     while (1) {
         int degrees = as5600_refresh(as);
-        int deltadegrees = degrees - lastDegree;
+        int deltadegrees = 0;
 
-        if (deltadegrees < -200) {
-            deltadegrees += 360 + 50;
-        } else if (deltadegrees > 200) {
-            deltadegrees -= 360 + 50;
+        if (degrees - lastDegree < -200) {
+            deltadegrees = (degrees - lastDegree) + 360 + 50;
+        } else if (degrees - lastDegree > 200) {
+            deltadegrees = (degrees - lastDegree) - 360 - 50;
+        } else {
+            deltadegrees = (degrees - lastDegree);
         }
 
-        int currentIdent = (degrees + 6 + IDENT_OFFSET) / 12;
-        if (lastIdent != currentIdent && currentIdent != 30) {
-            uint8_t key = deltadegrees > 0 ? KEY_1_CONFIGURE : KEY_2_CONFIGURE;
-            send_key_report(key);
-            lastIdent = currentIdent;
+        if (lastIdent != (((degrees + 6 + IDENT_OFFSET)) - ((degrees + 6 + IDENT_OFFSET) % 12)) / 12 &&
+            (((degrees + 6 + IDENT_OFFSET)) - ((degrees + 6 + IDENT_OFFSET) % 12)) / 12 != 30) {
+            uint8_t rep[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+            if (deltadegrees > 0) {
+                rep[7] = KEY_1_CONFIGURE;
+            } else {
+                rep[7] = KEY_2_CONFIGURE;
+            }
+
+            k_sem_take(&usb_sem, K_FOREVER);
+            int ret = hid_int_ep_write(hdev, rep, sizeof(rep), NULL);
+            k_sem_give(&my_sem);
+            lastIdent = ((degrees + 6 + IDENT_OFFSET) - ((degrees + 6 + IDENT_OFFSET) % 12)) / 12;
             lastDegree = degrees;
         }
     }
 }
 
-BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
-             "Console device is not ACM CDC UART device");
+BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart), "Console device is not ACM CDC UART device");
 
 #if defined(CONFIG_USB_DEVICE_STACK_NEXT)
-USBD_CONFIGURATION_DEFINE(config_1,
-                          USB_SCD_SELF_POWERED,
-                          200);
-
+USBD_CONFIGURATION_DEFINE(config_1, USB_SCD_SELF_POWERED, 200);
 USBD_DESC_LANG_DEFINE(sample_lang);
 USBD_DESC_MANUFACTURER_DEFINE(sample_mfr, "ZEPHYR");
 USBD_DESC_PRODUCT_DEFINE(sample_product, "Zephyr USBD ACM console");
 USBD_DESC_SERIAL_NUMBER_DEFINE(sample_sn, "0123456789ABCDEF");
 
-USBD_DEVICE_DEFINE(sample_usbd,
-                   DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0)),
-                   0x2fe3, 0x0001);
+USBD_DEVICE_DEFINE(sample_usbd, DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0)), 0x2fe3, 0x0001);
 
 static int enable_usb_device_next(void)
 {
@@ -121,6 +114,7 @@ static int enable_usb_device_next(void)
 
     return 0;
 }
+
 #endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK_NEXT) */
 
 int main(void)
@@ -143,11 +137,16 @@ int main(void)
                     threadA, NULL, NULL, NULL,
                     PRIORITY, 0, K_FOREVER);
     k_thread_name_set(&threadA_data, "thread_a");
+
     k_thread_start(&threadA_data);
 
     while (1) {
-        if (k_sem_take(&my_sem, K_MSEC(50)) == 0) {
-            send_key_report(0x00); // Release all keys
+        if (k_sem_take(&my_sem, K_MSEC(50)) != 0) {
+        } else {
+            uint8_t rep[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+            k_sem_take(&usb_sem, K_FOREVER);
+            int ret = hid_int_ep_write(hdev, rep, sizeof(rep), NULL);
         }
     }
 }
@@ -161,8 +160,8 @@ static int composite_pre_init(void)
     }
 
     usb_hid_register_device(hdev, hid_kbd_report_desc, sizeof(hid_kbd_report_desc), &ops);
-
     return usb_hid_init(hdev);
 }
 
 SYS_INIT(composite_pre_init, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
+```
