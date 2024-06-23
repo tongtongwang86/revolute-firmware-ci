44f1a8429c65f5722c4060d6425e0fa55f060725
Tong tong <100080262+tongtongwang86@users.noreply.github.com>
2024-06-23 11:44:02 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -28,10 +28,36 @@ static struct k_thread thread_data;
 static const struct device *hid_device;
 
 static const uint8_t hid_consumer_report_desc[] = {
+  const uint8_t hid_descriptor[] = {
+    // Consumer Control
+    0x05, 0x0C,       // USAGE_PAGE (Consumer Devices)
+    0x09, 0x01,       // USAGE (Consumer Control)
+    0xA1, 0x01,       // COLLECTION (Application)
+    0x85, 0x01,       //   REPORT_ID (1)
+    0x15, 0x00,       //   LOGICAL_MINIMUM (0)
+    0x25, 0x01,       //   LOGICAL_MAXIMUM (1)
+    0x75, 0x01,       //   REPORT_SIZE (1)
+    0x95, 0x03,       //   REPORT_COUNT (3)
+    0x09, 0xE2,       //   USAGE (Mute)
+    0x09, 0xE9,       //   USAGE (Volume Up)
+    0x09, 0xEA,       //   USAGE (Volume Down)
+    0x81, 0x02,       //   INPUT (Data,Var,Abs)
+    0x95, 0x01,       //   REPORT_COUNT (1)
+    0x75, 0x05,       //   REPORT_SIZE (5)
+    0x81, 0x03,       //   INPUT (Cnst,Var,Abs)
+    0x95, 0x01,       //   REPORT_COUNT (1)
+    0x75, 0x08,       //   REPORT_SIZE (8)
+    0x09, 0xB0,       //   USAGE (Play)
+    0x09, 0xB7,       //   USAGE (Stop)
+    0x09, 0xCD,       //   USAGE (Play/Pause)
+    0x81, 0x02,       //   INPUT (Data,Var,Abs)
+    0xC0,             // END_COLLECTION
+
     // Keyboard
     0x05, 0x01,       // USAGE_PAGE (Generic Desktop)
     0x09, 0x06,       // USAGE (Keyboard)
     0xA1, 0x01,       // COLLECTION (Application)
+    0x85, 0x02,       //   REPORT_ID (2)
     0x05, 0x07,       //   USAGE_PAGE (Keyboard)
     0x19, 0xE0,       //   USAGE_MINIMUM (Keyboard LeftControl)
     0x29, 0xE7,       //   USAGE_MAXIMUM (Keyboard Right GUI)
@@ -62,29 +88,9 @@ static const uint8_t hid_consumer_report_desc[] = {
     0x81, 0x00,       //   INPUT (Data,Ary,Abs)
     0xC0,             // END_COLLECTION
 
-    // Consumer Control
-    0x05, 0x0C,       // USAGE_PAGE (Consumer Devices)
-    0x09, 0x01,       // USAGE (Consumer Control)
-    0xA1, 0x01,       // COLLECTION (Application)
-    0x85, 0x01,       //   REPORT_ID (1)
-    0x15, 0x00,       //   LOGICAL_MINIMUM (0)
-    0x25, 0x01,       //   LOGICAL_MAXIMUM (1)
-    0x75, 0x01,       //   REPORT_SIZE (1)
-    0x95, 0x03,       //   REPORT_COUNT (3)
-    0x09, 0xE2,       //   USAGE (Mute)
-    0x09, 0xE9,       //   USAGE (Volume Up)
-    0x09, 0xEA,       //   USAGE (Volume Down)
-    0x81, 0x02,       //   INPUT (Data,Var,Abs)
-    0x95, 0x01,       //   REPORT_COUNT (1)
-    0x75, 0x05,       //   REPORT_SIZE (5)
-    0x81, 0x03,       //   INPUT (Cnst,Var,Abs)
-    0x95, 0x01,       //   REPORT_COUNT (1)
-    0x75, 0x08,       //   REPORT_SIZE (8)
-    0x09, 0xB0,       //   USAGE (Play)
-    0x09, 0xB7,       //   USAGE (Stop)
-    0x09, 0xCD,       //   USAGE (Play/Pause)
-    0x81, 0x02,       //   INPUT (Data,Var,Abs)
-    0xC0,             // END_COLLECTION
+
+};
+
 };
 
 K_SEM_DEFINE(data_ready_sem, 0, 10);
