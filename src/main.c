1de991bc7303a9ab847185537b488e9d17894c05
Tong tong <100080262+tongtongwang86@users.noreply.github.com>
2024-07-04 14:04:00 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,12 +6,94 @@
 #include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/gatt.h>
 #include <zephyr/mgmt/mcumgr/transport/smp_bt.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/logging/log_backend_ble.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS 2500
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+
+LOG_MODULE_REGISTER(ble_backend);
+
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, LOGGER_BACKEND_BLE_ADV_UUID_DATA),
+};
+static const struct bt_data sd[] = {
+	BT_DATA_BYTES(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME)};
+
+
+static void start_adv(void)
+{
+	int err;
+
+	err = bt_le_adv_start(BT_LE_ADV_CONN_ONE_TIME, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+
+	if (err) {
+		LOG_ERR("Advertising failed to start (err %d)", err);
+		return;
+	}
+
+	LOG_INF("Advertising successfully started");
+}
+
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	if (err) {
+		LOG_ERR("Connection failed (err 0x%02x)", err);
+	} else {
+		LOG_INF("Connected");
+	}
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	LOG_INF("Disconnected (reason 0x%02x)", reason);
+	start_adv();
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
+static void auth_cancel(struct bt_conn *conn)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	LOG_INF("Pairing cancelled: %s", addr);
+}
+
+static struct bt_conn_auth_cb auth_cb_display = {
+	.cancel = auth_cancel,
+};
+
+void backend_ble_hook(bool status, void *ctx)
+{
+	ARG_UNUSED(ctx);
+
+	if (status) {
+		LOG_INF("BLE Logger Backend enabled.");
+	} else {
+		LOG_INF("BLE Logger Backend disabled.");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
 
 /*
  * A build error on this line means your board is unsupported.
@@ -20,56 +102,47 @@
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 /* Register advertising data */
-static const struct bt_data ad[] = {
-	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-};
-static const struct bt_data sd[] = {
-	BT_DATA_BYTES(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME)};
 
 int main(void)
 {
-	int ret;
-
-	printk("Board: %s\n", CONFIG_BOARD);
-	printk("Build time: " __DATE__ " " __TIME__ "\n");
+	int err;
 
 	if (!gpio_is_ready_dt(&led))
 	{
 		return -ENODEV;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0)
+	err = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (err < 0)
 	{
 		return -ENODEV;
 	}
 
-	/* Enable Bluetooth */
-	ret = bt_enable(NULL);
-	if (ret < 0)
-	{
-		printk("Bluetooth init failed (err %d)\n", ret);
-		return ret;
+	
+	LOG_INF("BLE LOG Demo");
+	logger_backend_ble_set_hook(backend_ble_hook, NULL);
+	err = bt_enable(NULL);
+	if (err) {
+		LOG_ERR("Bluetooth init failed (err %d)", err);
+		return 0;
 	}
 
-	/* Start advertising */
-	ret = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad),
-						  sd, ARRAY_SIZE(sd));
-	if (ret < 0)
-	{
-		printk("Advertising failed to start (err %d)\n", ret);
-		return ret;
-	}
+	bt_conn_auth_cb_register(&auth_cb_display);
 
-	while (1)
-	{
+	start_adv();
+
+	while (1) {
+
 		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0)
 		{
 			return -ENODEV;
 		}
+		
+		uint32_t uptime_secs = k_uptime_get_32()/1000U;
+
+		LOG_INF("Uptime %d secs", uptime_secs);
 		k_msleep(SLEEP_TIME_MS);
 	}
-
 	return 0;
 }
