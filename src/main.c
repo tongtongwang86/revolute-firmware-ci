0bba3a134c86aecd052b736b879ba60ec669d673
Tong tong <100080262+tongtongwang86@users.noreply.github.com>
2024-07-05 11:58:42 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,25 +1,25 @@
-/*
- * Copyright (c) 2023 Victor Chavez
- *
- * SPDX-License-Identifier: Apache-2.0
- */
 
+
+
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/logging/log_backend_ble.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/conn.h>
 #include <zephyr/bluetooth/addr.h>
 #include <zephyr/bluetooth/hci.h>
-
-
-#include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/kernel.h>
 #include <zephyr/bluetooth/gatt.h>
-#include <zephyr/logging/log.h>
-#include <zephyr/logging/log_backend_ble.h>
 
 LOG_MODULE_REGISTER(ble_backend);
 
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA_BYTES(BT_DATA_UUID16_ALL,
+		      BT_UUID_16_ENCODE(BT_UUID_HIDS_VAL),
+		      BT_UUID_16_ENCODE(BT_UUID_BAS_VAL)),
 	BT_DATA_BYTES(BT_DATA_UUID128_ALL, LOGGER_BACKEND_BLE_ADV_UUID_DATA)
 };
 
