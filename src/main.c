5c60526268a14d5b70765958656ec19eeb0fd722
Tong tong <100080262+tongtongwang86@users.noreply.github.com>
2024-07-05 10:21:26 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,37 +1,31 @@
+/*
+ * Copyright (c) 2023 Victor Chavez
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/bluetooth/conn.h>
-#include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/gatt.h>
-#include <zephyr/mgmt/mcumgr/transport/smp_bt.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/logging/log_backend_ble.h>
-
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS 2500
-
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
 
 LOG_MODULE_REGISTER(ble_backend);
 
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-	BT_DATA_BYTES(BT_DATA_UUID128_ALL, LOGGER_BACKEND_BLE_ADV_UUID_DATA),
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, LOGGER_BACKEND_BLE_ADV_UUID_DATA)
 };
-static const struct bt_data sd[] = {
-	BT_DATA_BYTES(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME)};
 
+static const struct bt_data sd[] = {
+	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
+};
 
 static void start_adv(void)
 {
 	int err;
 
-	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
-
+	err = bt_le_adv_start(BT_LE_ADV_CONN_ONE_TIME, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
 	if (err) {
 		LOG_ERR("Advertising failed to start (err %d)", err);
 		return;
@@ -85,40 +79,10 @@ void backend_ble_hook(bool status, void *ctx)
 }
 
 
-
-
-
-
-
-
-
-
-
-
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-
-/* Register advertising data */
-
 int main(void)
 {
 	int err;
 
-	if (!gpio_is_ready_dt(&led))
-	{
-		return -ENODEV;
-	}
-
-	err = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (err < 0)
-	{
-		return -ENODEV;
-	}
-
-	
 	LOG_INF("BLE LOG Demo");
 	logger_backend_ble_set_hook(backend_ble_hook, NULL);
 	err = bt_enable(NULL);
@@ -132,17 +96,10 @@ int main(void)
 	start_adv();
 
 	while (1) {
-
-		err = gpio_pin_toggle_dt(&led);
-		if (err < 0)
-		{
-			return -ENODEV;
-		}
-		
 		uint32_t uptime_secs = k_uptime_get_32()/1000U;
 
 		LOG_INF("Uptime %d secs", uptime_secs);
-		k_msleep(SLEEP_TIME_MS);
+		k_sleep(K_MSEC(1000));
 	}
 	return 0;
 }
