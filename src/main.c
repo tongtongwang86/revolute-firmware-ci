f9c29e19200b1c520dc439150d9fc6de67c15b8a
Tong tong <100080262+tongtongwang86@users.noreply.github.com>
2024-07-06 12:44:13 +0800

    change 2

--- src/main.c
+++ src/main.c
@@ -5,10 +5,13 @@
 
 
 
+
+#include <zephyr/settings/settings.h>
+#include <zephyr/drivers/gpio.h>
+
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/logging/log_backend_ble.h>
-
 
 #include <zephyr/mgmt/mcumgr/transport/smp_bt.h>
 #include <zephyr/bluetooth/bluetooth.h>
@@ -18,9 +21,24 @@
 #include <zephyr/bluetooth/gatt.h>
 #include <zephyr/bluetooth/uuid.h>
 
-#include <zephyr/settings/settings.h>
-
 LOG_MODULE_REGISTER(ble_backend);
+
+#define SW0_NODE	DT_ALIAS(sw0) 
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+
+static struct gpio_callback button_cb_data;
+
+#define BT_LE_ADV_CONN_NO_ACCEPT_LIST                                                              \
+	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE | BT_LE_ADV_OPT_ONE_TIME,                        \
+			BT_GAP_ADV_FAST_INT_MIN_2, BT_GAP_ADV_FAST_INT_MAX_2, NULL)
+/* STEP 3.2.2 - Define advertising parameter for when Accept List is used */
+#define BT_LE_ADV_CONN_ACCEPT_LIST                                                                 \
+	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE | BT_LE_ADV_OPT_FILTER_CONN |                    \
+				BT_LE_ADV_OPT_ONE_TIME,                                            \
+			BT_GAP_ADV_FAST_INT_MIN_2, BT_GAP_ADV_FAST_INT_MAX_2, NULL)
+
+
+
 
 
 static const struct bt_data ad[] = {
@@ -35,19 +53,81 @@ static const struct bt_data sd[] = {
 	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
 };
 
-static void start_adv(void)
-{	
-
+// static void start_adv(void)
+// {	
 	
-	int err;
-	err = bt_le_adv_start(BT_LE_ADV_CONN_ONE_TIME, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
-	if (err) {
-		LOG_ERR("Advertising failed to start (err %d)", err);
+	
+// 	int err;
+// 	err = bt_le_adv_start(adv_param, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+// 	if (err) {
+// 		LOG_ERR("Advertising failed to start (err %d)", err);
+// 		return;
+// 	}
+
+// 	LOG_INF("Advertising successfully started");
+// }
+
+static void setup_accept_list_cb(const struct bt_bond_info *info, void *user_data)
+{
+	int *bond_cnt = user_data;
+
+	if ((*bond_cnt) < 0) {
 		return;
 	}
 
-	LOG_INF("Advertising successfully started");
+	int err = bt_le_filter_accept_list_add(&info->addr);
+	LOG_INF("Added following peer to accept list: %x %x\n", info->addr.a.val[0],
+		info->addr.a.val[1]);
+	if (err) {
+		LOG_INF("Cannot add peer to filter accept list (err: %d)\n", err);
+		(*bond_cnt) = -EIO;
+	} else {
+		(*bond_cnt)++;
+	}
 }
+
+/* STEP 3.3.2 - Define the function to loop through the bond list */
+static int setup_accept_list(uint8_t local_id)
+{
+	int err = bt_le_filter_accept_list_clear();
+
+	if (err) {
+		LOG_INF("Cannot clear accept list (err: %d)\n", err);
+		return err;
+	}
+
+	int bond_cnt = 0;
+
+	bt_foreach_bond(local_id, setup_accept_list_cb, &bond_cnt);
+
+	return bond_cnt;
+}
+
+void advertise_with_acceptlist(struct k_work *work)
+{
+	int err = 0;
+	int allowed_cnt = setup_accept_list(BT_ID_DEFAULT);
+	if (allowed_cnt < 0) {
+		LOG_INF("Acceptlist setup failed (err:%d)\n", allowed_cnt);
+	} else {
+		if (allowed_cnt == 0) {
+			LOG_INF("Advertising with no Accept list \n");
+			err = bt_le_adv_start(BT_LE_ADV_CONN_NO_ACCEPT_LIST, ad, ARRAY_SIZE(ad), sd,
+					      ARRAY_SIZE(sd));
+		} else {
+			LOG_INF("Acceptlist setup number  = %d \n", allowed_cnt);
+			err = bt_le_adv_start(BT_LE_ADV_CONN_ACCEPT_LIST, ad, ARRAY_SIZE(ad), sd,
+					      ARRAY_SIZE(sd));
+		}
+		if (err) {
+			LOG_INF("Advertising failed to start (err %d)\n", err);
+			return;
+		}
+		LOG_INF("Advertising successfully started\n");
+	}
+}
+K_WORK_DEFINE(advertise_acceptlist_work, advertise_with_acceptlist);
+
 
 static void connected(struct bt_conn *conn, uint8_t err)
 {
@@ -67,14 +147,10 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 {	
 	int err;
 	LOG_INF("Disconnected (reason 0x%02x)", reason);
-	start_adv();
+
+	k_work_submit(&advertise_acceptlist_work);
 	
-	 err = bt_unpair(BT_ID_DEFAULT, BT_ADDR_LE_ANY);
-			if (err) {
-				LOG_INF("Cannot delete bond (err: %d)\n", err);
-			} else {
-				LOG_INF("Bond deleted succesfully");
-			}
+	// ¡¡™™¡™™¡
 	
 }
 
@@ -123,12 +199,59 @@ void backend_ble_hook(bool status, void *ctx)
 	}
 }
 
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+    int err= bt_unpair(BT_ID_DEFAULT,BT_ADDR_LE_ANY);
+	if (err) {
+		LOG_INF("Cannot delete bond (err: %d)\n", err);
+	} else	{
+		LOG_INF("Bond deleted succesfully \n");
+	}		
+	err = bt_le_adv_stop();
+	if (err) {
+		LOG_INF("Cannot stop advertising err= %d \n", err);
+		return;
+	}
+	err = bt_le_filter_accept_list_clear();
+	if (err) {
+		LOG_INF("Cannot clear accept list (err: %d)\n", err);
+	} else {
+		LOG_INF("Accept list cleared succesfully");
+	}
+	err = bt_le_adv_start(BT_LE_ADV_CONN_NO_ACCEPT_LIST, ad,
+						   ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+
+	if (err) {
+		LOG_INF("Cannot start open advertising (err: %d)\n", err);
+	} else {
+		LOG_INF("Advertising in pairing mode started");
+	}
+	
+}
+
 
 int main(void)
 {
 	int err;
+	
 
 	LOG_INF("BLE LOG Demo");
+
+	if (!device_is_ready(button.port)) {
+	return -1;
+	}
+	
+	err = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (err < 0) {
+		return -1;
+	}
+
+	err = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	
+  	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin)); 	
+	gpio_add_callback(button.port, &button_cb_data);
+
+	
 	logger_backend_ble_set_hook(backend_ble_hook, NULL);
 	err = bt_enable(NULL);
 	if (err) {
@@ -139,7 +262,8 @@ int main(void)
 
 	bt_conn_auth_cb_register(&auth_cb_display);
 
-	start_adv();
+	// start_adv();
+	k_work_submit(&advertise_acceptlist_work);
 
 	while (1) {
 		uint32_t uptime_secs = k_uptime_get_32()/1000U;
