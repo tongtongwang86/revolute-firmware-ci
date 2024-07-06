ce606620b65ef5265ecff0ed3f907ba3dd2c11b4
Tong tong <100080262+tongtongwang86@users.noreply.github.com>
2024-07-06 15:20:42 +0800

    button debounce

--- src/main.c
+++ src/main.c
@@ -1,275 +1,119 @@
-#define BT_LE_ADV_CONN_ONE_TIME \
-	  BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE | BT_LE_ADV_OPT_ONE_TIME,                        \
-                        BT_GAP_ADV_FAST_INT_MIN_2, BT_GAP_ADV_FAST_INT_MAX_2, NULL)
-
-
-
-
-
-#include <zephyr/settings/settings.h>
-#include <zephyr/drivers/gpio.h>
-
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
-#include <zephyr/logging/log_backend_ble.h>
+#define SW0_NODE	DT_ALIAS(sw0)
+#define LED0_NODE DT_ALIAS(led0)
 
-#include <zephyr/mgmt/mcumgr/transport/smp_bt.h>
-#include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/bluetooth/conn.h>
-#include <zephyr/bluetooth/addr.h>
-#include <zephyr/bluetooth/hci.h>
-#include <zephyr/bluetooth/gatt.h>
-#include <zephyr/bluetooth/uuid.h>
 
-LOG_MODULE_REGISTER(ble_backend);
-
-#define SW0_NODE	DT_ALIAS(sw0) 
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
-
 static struct gpio_callback button_cb_data;
 
-#define BT_LE_ADV_CONN_NO_ACCEPT_LIST                                                              \
-	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE | BT_LE_ADV_OPT_ONE_TIME,                        \
-			BT_GAP_ADV_FAST_INT_MIN_2, BT_GAP_ADV_FAST_INT_MAX_2, NULL)
-/* STEP 3.2.2 - Define advertising parameter for when Accept List is used */
-#define BT_LE_ADV_CONN_ACCEPT_LIST                                                                 \
-	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE | BT_LE_ADV_OPT_FILTER_CONN |                    \
-				BT_LE_ADV_OPT_ONE_TIME,                                            \
-			BT_GAP_ADV_FAST_INT_MIN_2, BT_GAP_ADV_FAST_INT_MAX_2, NULL)
+static button_event_handler_t user_cb;
 
-
-
-
-
-static const struct bt_data ad[] = {
-	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-	BT_DATA_BYTES(BT_DATA_UUID16_ALL,
-		      BT_UUID_16_ENCODE(BT_UUID_HIDS_VAL),
-		      BT_UUID_16_ENCODE(BT_UUID_BAS_VAL)),
-	BT_DATA_BYTES(BT_DATA_UUID128_ALL, LOGGER_BACKEND_BLE_ADV_UUID_DATA)
-};
-
-static const struct bt_data sd[] = {
-	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
-};
-
-// static void start_adv(void)
-// {	
-	
-	
-// 	int err;
-// 	err = bt_le_adv_start(adv_param, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
-// 	if (err) {
-// 		LOG_ERR("Advertising failed to start (err %d)", err);
-// 		return;
-// 	}
-
-// 	LOG_INF("Advertising successfully started");
-// }
-
-static void setup_accept_list_cb(const struct bt_bond_info *info, void *user_data)
+static void cooldown_expired(struct k_work *work)
 {
-	int *bond_cnt = user_data;
+    ARG_UNUSED(work);
 
-	if ((*bond_cnt) < 0) {
-		return;
-	}
-
-	int err = bt_le_filter_accept_list_add(&info->addr);
-	LOG_INF("Added following peer to accept list: %x %x\n", info->addr.a.val[0],
-		info->addr.a.val[1]);
-	if (err) {
-		LOG_INF("Cannot add peer to filter accept list (err: %d)\n", err);
-		(*bond_cnt) = -EIO;
-	} else {
-		(*bond_cnt)++;
-	}
+    int val = gpio_pin_get_dt(&button);
+    enum button_evt evt = val ? BUTTON_EVT_PRESSED : BUTTON_EVT_RELEASED;
+    if (user_cb) {
+        user_cb(evt);
+    }
 }
 
-/* STEP 3.3.2 - Define the function to loop through the bond list */
-static int setup_accept_list(uint8_t local_id)
-{
-	int err = bt_le_filter_accept_list_clear();
+static K_WORK_DELAYABLE_DEFINE(cooldown_work, cooldown_expired);
 
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+		    uint32_t pins)
+{
+    k_work_reschedule(&cooldown_work, K_MSEC(15));
+}
+
+
+
+
+int button_init(button_event_handler_t handler)
+{
+    int err = -1;
+
+    if (!handler) {
+        return -EINVAL;
+    }
+
+    user_cb = handler;
+
+	if (!device_is_ready(button.port)) {
+		return -EIO;
+	}
+
+	err = gpio_pin_configure_dt(&button, GPIO_INPUT);
 	if (err) {
-		LOG_INF("Cannot clear accept list (err: %d)\n", err);
+        return err;
+	}
+
+	err = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_BOTH);
+	if (err) {
 		return err;
 	}
 
-	int bond_cnt = 0;
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	err = gpio_add_callback(button.port, &button_cb_data);
+    if (err) {
+        return err;
+    }
 
-	bt_foreach_bond(local_id, setup_accept_list_cb, &bond_cnt);
-
-	return bond_cnt;
+    return 0;
 }
 
-void advertise_with_acceptlist(struct k_work *work)
+static char *helper_button_evt_str(enum button_evt evt)
 {
-	int err = 0;
-	int allowed_cnt = setup_accept_list(BT_ID_DEFAULT);
-	if (allowed_cnt < 0) {
-		LOG_INF("Acceptlist setup failed (err:%d)\n", allowed_cnt);
-	} else {
-		if (allowed_cnt == 0) {
-			LOG_INF("Advertising with no Accept list \n");
-			err = bt_le_adv_start(BT_LE_ADV_CONN_NO_ACCEPT_LIST, ad, ARRAY_SIZE(ad), sd,
-					      ARRAY_SIZE(sd));
-		} else {
-			LOG_INF("Acceptlist setup number  = %d \n", allowed_cnt);
-			err = bt_le_adv_start(BT_LE_ADV_CONN_ACCEPT_LIST, ad, ARRAY_SIZE(ad), sd,
-					      ARRAY_SIZE(sd));
-		}
-		if (err) {
-			LOG_INF("Advertising failed to start (err %d)\n", err);
-			return;
-		}
-		LOG_INF("Advertising successfully started\n");
-	}
-}
-K_WORK_DEFINE(advertise_acceptlist_work, advertise_with_acceptlist);
-
-
-static void connected(struct bt_conn *conn, uint8_t err)
-{
-	if (err) {
-		LOG_ERR("Connection failed (err 0x%02x)", err);
-	} else {
-		LOG_INF("Connected");
-	}
-
-		if (bt_conn_set_security(conn, BT_SECURITY_L2)) {
-		LOG_INF("Failed to set security\n");
-	}
-	
-}
-
-static void disconnected(struct bt_conn *conn, uint8_t reason)
-{	
 	int err;
-	LOG_INF("Disconnected (reason 0x%02x)", reason);
-
-	k_work_submit(&advertise_acceptlist_work);
+	switch (evt) {
+	case BUTTON_EVT_PRESSED:
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return 0;
+		}
 	
-	// ¡¡™™¡™™¡
-	
-}
-
-static void security_changed(struct bt_conn *conn, bt_security_t level,
-			     enum bt_security_err err)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	if (!err) {
-		LOG_INF("Security changed: %s level %u\n", addr, level);
-	} else {
-		LOG_INF("Security failed: %s level %u err %d\n", addr, level,
-		       err);
+		return "Pressed";
+	case BUTTON_EVT_RELEASED:
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return 0;
+		}
+		
+		return "Released";
+	default:
+		return "Unknown";
 	}
 }
 
-BT_CONN_CB_DEFINE(conn_callbacks) = {
-	.connected = connected,
-	.disconnected = disconnected,
-	.security_changed = security_changed,
-};
-
-static void auth_cancel(struct bt_conn *conn)
+static void button_event_handler(enum button_evt evt)
 {
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	LOG_INF("Pairing cancelled: %s", addr);
+	printk("Button event: %s\n", helper_button_evt_str(evt));
 }
 
-static struct bt_conn_auth_cb auth_cb_display = {
-	.cancel = auth_cancel,
-};
-
-void backend_ble_hook(bool status, void *ctx)
+void main(void)
 {
-	ARG_UNUSED(ctx);
+	int err = -1;
 
-	if (status) {
-		LOG_INF("BLE Logger Backend enabled.");
-	} else {
-		LOG_INF("BLE Logger Backend disabled.");
-	}
-}
+	printk("Button Debouncing Sample!\n");
 
-void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
-{
-    int err= bt_unpair(BT_ID_DEFAULT,BT_ADDR_LE_ANY);
+	err = button_init(button_event_handler);
 	if (err) {
-		LOG_INF("Cannot delete bond (err: %d)\n", err);
-	} else	{
-		LOG_INF("Bond deleted succesfully \n");
-	}		
-	err = bt_le_adv_stop();
-	if (err) {
-		LOG_INF("Cannot stop advertising err= %d \n", err);
+		printk("Button Init failed: %d\n", err);
 		return;
 	}
-	err = bt_le_filter_accept_list_clear();
-	if (err) {
-		LOG_INF("Cannot clear accept list (err: %d)\n", err);
-	} else {
-		LOG_INF("Accept list cleared succesfully");
-	}
-	err = bt_le_adv_start(BT_LE_ADV_CONN_NO_ACCEPT_LIST, ad,
-						   ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
 
-	if (err) {
-		LOG_INF("Cannot start open advertising (err: %d)\n", err);
-	} else {
-		LOG_INF("Advertising in pairing mode started");
-	}
-	
-}
-
-
-int main(void)
-{
-	int err;
-	
-
-	LOG_INF("BLE LOG Demo");
-
-	if (!device_is_ready(button.port)) {
-	return -1;
-	}
-	
-	err = gpio_pin_configure_dt(&button, GPIO_INPUT);
-	if (err < 0) {
-		return -1;
-	}
-
-	err = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
-	
-  	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin)); 	
-	gpio_add_callback(button.port, &button_cb_data);
-
-	
-	logger_backend_ble_set_hook(backend_ble_hook, NULL);
-	err = bt_enable(NULL);
-	if (err) {
-		LOG_ERR("Bluetooth init failed (err %d)", err);
+	if (!gpio_is_ready_dt(&led)) {
 		return 0;
 	}
-	settings_load();
 
-	bt_conn_auth_cb_register(&auth_cb_display);
-
-	// start_adv();
-	k_work_submit(&advertise_acceptlist_work);
-
-	while (1) {
-		uint32_t uptime_secs = k_uptime_get_32()/1000U;
-
-		LOG_INF("Uptime %d secs", uptime_secs);
-		k_sleep(K_MSEC(1000));
+	err = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (err < 0) {
+		return 0;
 	}
-	return 0;
+
+	
+
+	printk("Init succeeded. Waiting for event...\n");
 }
