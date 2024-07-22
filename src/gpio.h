96fd56c3f7d2aeb783b17be9ba8c745b6a647e0b
Tong tong <t0ngx2@icloud.com>
2024-07-22 21:42:59 +0800

    adv conn works with button 3 deleting the bond

--- src/gpio.h
+++ src/gpio.h
@@ -6,15 +6,29 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/logging/log.h>
 
+// GPIO device tree node identifiers
 #define SW0_NODE DT_ALIAS(sw0)
 #define SW1_NODE DT_ALIAS(sw1)
 #define SW2_NODE DT_ALIAS(sw2)
 #define SW3_NODE DT_ALIAS(sw3)
 #define LED0_NODE DT_ALIAS(led0)
 
-// Declare the led variable as extern so it can be accessed in other files
+// External declarations for the led variable
 extern const struct gpio_dt_spec led;
 
+extern const struct gpio_dt_spec buttons[];
+
 int ledInit(void);
+
+// Button event definitions
+enum button_evt {
+    BUTTON_EVT_PRESSED,
+    BUTTON_EVT_RELEASED
+};
+
+typedef void (*button_event_handler_t)(size_t idx, enum button_evt evt);
+
+// Function declarations for button handling
+int button_init(size_t idx, button_event_handler_t handler);
 
 #endif // GPIO_H
--- src/main.c
+++ src/main.c
@@ -6,17 +6,73 @@
 
 LOG_MODULE_REGISTER(Revolute, LOG_LEVEL_DBG);
 
+// Event handler for button events
+static void button_event_handler(size_t idx, enum button_evt evt)
+{
+    int err;
+    switch (idx)
+    {
+    case 0:
+        if (evt == BUTTON_EVT_PRESSED) {
+            LOG_INF("Button 0 pressed");
+        } else {
+            LOG_INF("Button 0 released");
+        }
+        break;
+    case 1:
+        if (evt == BUTTON_EVT_PRESSED) {
+            err = gpio_pin_toggle_dt(&led);
+            if (err < 0) {
+                return;
+            }
+            LOG_INF("Button 1 pressed");
+        } else {
+            LOG_INF("Button 1 released");
+        }
+        break;
+    case 2:
+        if (evt == BUTTON_EVT_PRESSED) {
+            LOG_INF("Button 2 pressed");
+        } else {
+            err = gpio_pin_toggle_dt(&led);
+            if (err < 0) {
+                return;
+            }
+            LOG_INF("Button 2 released");
+        }
+        break;
+    case 3:
+        if (evt == BUTTON_EVT_PRESSED) {
+            deleteBond();
+            LOG_INF("Button 3 pressed");
+        } else {
+            LOG_INF("Button 3 released");
+        }
+        break;
+    default:
+        LOG_ERR("Unknown button %zu event", idx);
+        break;
+    }
+}
+
 int main(void)
 {
+    // Initialize LEDs
     ledInit();
+
+    // Initialize BLE
     enableBle();
     startAdv();
+
+    // Initialize buttons
+    for (size_t i = 0; i < 4; i++) {
+        button_init(i, button_event_handler);
+    }
 
     while (1)
     {
         int err = gpio_pin_toggle_dt(&led);
-        if (err < 0)
-        {
+        if (err < 0) {
             return 0;
         }
 
