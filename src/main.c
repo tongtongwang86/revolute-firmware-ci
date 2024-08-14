e263d61f80aaf5c62448f8559915da4e38f38c3e
Tong tong <t0ngx2@icloud.com>
2024-08-14 10:03:40 +0800

    gyro sample test

--- src/main.c
+++ src/main.c
@@ -9,32 +9,122 @@ LOG_MODULE_REGISTER(Revolute, LOG_LEVEL_DBG);
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/sys/printk.h>
 
-#define I2C_NODE DT_NODELABEL(i2c0) // Referencing the i2c0 node
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
 
-void main(void) {
-    const struct device *i2c_dev = DEVICE_DT_GET(I2C_NODE);
+static inline float out_ev(struct sensor_value *val)
+{
+	return (val->val1 + (float)val->val2 / 1000000);
+}
 
-    if (!device_is_ready(i2c_dev)) {
-        printk("I2C device not found\n");
-        return;
-    }
+static void fetch_and_display(const struct device *dev)
+{
+	struct sensor_value x, y, z;
+	static int trig_cnt;
 
-    printk("I2C device found: %s\n", i2c_dev->name);
+	trig_cnt++;
 
-    for (uint8_t addr = 0x03; addr <= 0x77; addr++) {
-        struct i2c_msg msgs[1];
-        uint8_t dummy = 0;
+	/* lsm6dso accel */
+	sensor_sample_fetch_chan(dev, SENSOR_CHAN_ACCEL_XYZ);
+	sensor_channel_get(dev, SENSOR_CHAN_ACCEL_X, &x);
+	sensor_channel_get(dev, SENSOR_CHAN_ACCEL_Y, &y);
+	sensor_channel_get(dev, SENSOR_CHAN_ACCEL_Z, &z);
 
-        msgs[0].buf = &dummy;
-        msgs[0].len = 1U;
-        msgs[0].flags = I2C_MSG_WRITE | I2C_MSG_STOP;
+	printf("accel x:%f ms/2 y:%f ms/2 z:%f ms/2\n",
+			(double)out_ev(&x), (double)out_ev(&y), (double)out_ev(&z));
 
-        int ret = i2c_transfer(i2c_dev, msgs, 1, addr);
+	/* lsm6dso gyro */
+	sensor_sample_fetch_chan(dev, SENSOR_CHAN_GYRO_XYZ);
+	sensor_channel_get(dev, SENSOR_CHAN_GYRO_X, &x);
+	sensor_channel_get(dev, SENSOR_CHAN_GYRO_Y, &y);
+	sensor_channel_get(dev, SENSOR_CHAN_GYRO_Z, &z);
 
-        if (ret == 0) {
-            printk("I2C device found at address: 0x%02X\n", addr);
-        }
-    }
+	printf("gyro x:%f rad/s y:%f rad/s z:%f rad/s\n",
+			(double)out_ev(&x), (double)out_ev(&y), (double)out_ev(&z));
 
-    printk("I2C scan complete.\n");
+	printf("trig_cnt:%d\n\n", trig_cnt);
+}
+
+static int set_sampling_freq(const struct device *dev)
+{
+	int ret = 0;
+	struct sensor_value odr_attr;
+
+	/* set accel/gyro sampling frequency to 12.5 Hz */
+	odr_attr.val1 = 12.5;
+	odr_attr.val2 = 0;
+
+	ret = sensor_attr_set(dev, SENSOR_CHAN_ACCEL_XYZ,
+			SENSOR_ATTR_SAMPLING_FREQUENCY, &odr_attr);
+	if (ret != 0) {
+		printf("Cannot set sampling frequency for accelerometer.\n");
+		return ret;
+	}
+
+	ret = sensor_attr_set(dev, SENSOR_CHAN_GYRO_XYZ,
+			SENSOR_ATTR_SAMPLING_FREQUENCY, &odr_attr);
+	if (ret != 0) {
+		printf("Cannot set sampling frequency for gyro.\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+#ifdef CONFIG_LSM6DSO_TRIGGER
+static void trigger_handler(const struct device *dev,
+			    const struct sensor_trigger *trig)
+{
+	fetch_and_display(dev);
+}
+
+static void test_trigger_mode(const struct device *dev)
+{
+	struct sensor_trigger trig;
+
+	if (set_sampling_freq(dev) != 0)
+		return;
+
+	trig.type = SENSOR_TRIG_DATA_READY;
+	trig.chan = SENSOR_CHAN_ACCEL_XYZ;
+
+	if (sensor_trigger_set(dev, &trig, trigger_handler) != 0) {
+		printf("Could not set sensor type and channel\n");
+		return;
+	}
+}
+
+#else
+static void test_polling_mode(const struct device *dev)
+{
+	if (set_sampling_freq(dev) != 0) {
+		return;
+	}
+
+	while (1) {
+		fetch_and_display(dev);
+		k_sleep(K_MSEC(1000));
+	}
+}
+#endif
+
+int main(void)
+{
+	const struct device *const dev = DEVICE_DT_GET_ONE(st_lsm6dso);
+
+	if (!device_is_ready(dev)) {
+		printk("%s: device not ready.\n", dev->name);
+		return 0;
+	}
+
+#ifdef CONFIG_LSM6DSO_TRIGGER
+	printf("Testing LSM6DSO sensor in trigger mode.\n\n");
+	test_trigger_mode(dev);
+#else
+	printf("Testing LSM6DSO sensor in polling mode.\n\n");
+	test_polling_mode(dev);
+#endif
+	return 0;
 }
