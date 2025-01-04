983ec6f4990b1c9fc739f950106f89183920d14b
Tong tong <t0ngx2@icloud.com>
2025-01-04 21:55:34 +0800

    removed nrfconnect

--- src/main.c
+++ src/main.c
@@ -23,15 +23,6 @@ static void button_event_handler(size_t idx, enum button_evt evt)
         if (evt == BUTTON_EVT_PRESSED)
         {
 
-            struct hid_mouse_report_body report = {
-
-                .buttons = 0b00000001,
-                .d_x = 0x00,
-                .d_y = 0x00,
-                .d_wheel = 0x00,
-            };
-
-            mouse_bt_submit(&report);
 
             LOG_INF("Button 0 pressed");
         }
@@ -39,15 +30,6 @@ static void button_event_handler(size_t idx, enum button_evt evt)
         {
             LOG_INF("Button 0 released");
 
-            struct hid_mouse_report_body report = {
-
-                .buttons = 0b00000000,
-                .d_x = 0x00,
-                .d_y = 0x00,
-                .d_wheel = 0x00,
-            };
-
-            mouse_bt_submit(&report);
         }
         break;
     case 1:
@@ -59,25 +41,12 @@ static void button_event_handler(size_t idx, enum button_evt evt)
                 return;
             }
 
-            struct hid_keyboard_report_body report = {
-
-                .modifiers = 0b10000000, // | RGUI| RALT| RSHIFT| RCONTROL | LGUI| LALT| LSHIFT| LCONTROL|
-                .keys = HID_KEY_Z,
-
-            };
-            keyboard_bt_submit(&report);
 
             LOG_INF("Button 1 pressed");
         }
         else
         {
-            struct hid_keyboard_report_body report = {
-
-                .modifiers = 0b00000000, // | RGUI| RALT| RSHIFT| RCONTROL | LGUI| LALT| LSHIFT| LCONTROL|
-                .keys = 0,
-
-            };
-            keyboard_bt_submit(&report);
+           
 
             LOG_INF("Button 1 released");
         }
@@ -85,19 +54,13 @@ static void button_event_handler(size_t idx, enum button_evt evt)
     case 2:
         if (evt == BUTTON_EVT_PRESSED)
         {
-            struct hid_consumer_report_body report = {
-                .keys = HID_USAGE_CONSUMER_VOLUME_DECREMENT,
-            };
-            consumer_bt_submit(&report);
+           
 
             LOG_INF("Button 2 pressed");
         }
         else
         {
-            struct hid_consumer_report_body report = {
-                .keys = 0,
-            };
-            consumer_bt_submit(&report);
+           
             LOG_INF("Button 2 released");
         }
         break;
@@ -135,11 +98,11 @@ int main(void)
         button_init(i, button_event_handler);
     }
 
-    k_work_init(&keyboard_bt_action_work, keyboard_bt_action_work_handler); // start button work queue
-    k_work_init(&mouse_bt_action_work, mouse_bt_action_work_handler);       // start button work queue
-    k_work_init(&consumer_bt_action_work, consumer_bt_action_work_handler); // start button work queue
-    k_work_init(&revolute_bt_action_work, revolute_bt_action_work_handler); // start button work queue
-    SensorThreadinit();
+    // k_work_init(&keyboard_bt_action_work, keyboard_bt_action_work_handler); // start button work queue
+    // k_work_init(&mouse_bt_action_work, mouse_bt_action_work_handler);       // start button work queue
+    // k_work_init(&consumer_bt_action_work, consumer_bt_action_work_handler); // start button work queue
+    // k_work_init(&revolute_bt_action_work, revolute_bt_action_work_handler); // start button work queue
+    // SensorThreadinit();
     while (1)
     {
         int err = gpio_pin_toggle_dt(&led);
@@ -147,8 +110,6 @@ int main(void)
         {
             return 0;
         }
-
-        // LOG_INF("a");
 
         k_msleep(1000);
     }
