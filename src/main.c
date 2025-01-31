978e4697fac831f87231425bf47e435cfcab524d
Tong tong <t0ngx2@icloud.com>
2025-01-31 14:45:13 +0800

    led lane test for the revopod

--- src/main.c
+++ src/main.c
@@ -1,43 +1,66 @@
-#include <zephyr/kernel.h>
+#include <errno.h>
+#include <string.h>
+
+#define LOG_LEVEL 4
 #include <zephyr/logging/log.h>
-#include <zephyr/pm/pm.h>
-#include <zephyr/pm/policy.h>
-#include <zephyr/pm/state.h>
-#include <zephyr/pm/device.h>
-#include "hog.h"
-#include "revsvc.h"
-#include "batterylvl.h"
-#include "button.h"
-#include "ble.h"
-#include "settings.h"
-#include "led.h"
-#include "magnetic.h"
+LOG_MODULE_REGISTER(main);
 
-LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/led_strip.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/sys/util.h>
 
+#define STRIP_NODE DT_ALIAS(led_strip)
+
+#if DT_NODE_HAS_PROP(DT_ALIAS(led_strip), chain_length)
+#define STRIP_NUM_PIXELS	DT_PROP(DT_ALIAS(led_strip), chain_length)
+#else
+#error Unable to determine length of LED strip
+#endif
+
+#define DELAY_TIME K_MSEC(2000)
+
+#define RGB(_r, _g, _b) { .r = (_r), .g = (_g), .b = (_b) }
+
+static const struct led_rgb colors[] = {
+	RGB(0x0f, 0x00, 0x00), /* red */
+	RGB(0x00, 0x0f, 0x00), /* green */
+	RGB(0x00, 0x00, 0x0f), /* blue */
+};
+
+static struct led_rgb pixels[STRIP_NUM_PIXELS];
+
+static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
 
 int main(void)
 {
+	size_t color = 0;
+	int rc;
 
+	if (device_is_ready(strip)) {
+		LOG_INF("Found LED strip device %s", strip->name);
+	} else {
+		LOG_ERR("LED strip device %s is not ready", strip->name);
+		return 0;
+	}
 
-    bluetooth_init();
+	LOG_INF("Displaying pattern on strip");
+	while (1) {
+		for (size_t cursor = 0; cursor < ARRAY_SIZE(pixels); cursor++) {
+			memset(&pixels, 0x00, sizeof(pixels));
+			memcpy(&pixels[cursor], &colors[color], sizeof(struct led_rgb));
 
-    LOG_INF("rev_svc_loop thread started\n");
+			rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
+			if (rc) {
+				LOG_ERR("couldn't update strip: %d", rc);
+			}
 
-    batteryThreadinit();
-    rev_svc_thread_init();
-    button_init();
+			k_sleep(DELAY_TIME);
+		}
 
+		color = (color + 1) % ARRAY_SIZE(colors);
+	}
 
-
-    SensorThreadinit();
-
-
-	int err = pwmled_init();
-    if (err < 0) {
-        LOG_ERR("LED initialization failed (err %d)", err);
-        return err;
-    }
-
-    return 0;
+	return 0;
 }
