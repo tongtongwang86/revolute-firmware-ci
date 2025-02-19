72379b40afa85bd2e7e95f8b31ae0b49dd87a16d
fnckingsnowman <nedzhong@hotmail.com>
2025-02-19 17:52:44 +0800

    blinky works

--- src/main.c
+++ src/main.c
@@ -1,65 +1,45 @@
 /*
- * Copyright (c) 2016 Intel Corporation.
+ * Copyright (c) 2016 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <sample_usbd.h>
-
-#include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/usb/usb_device.h>
-#include <zephyr/usb/usbd.h>
-#include <zephyr/drivers/uart.h>
-
-BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
-	     "Console device is not ACM CDC UART device");
-
-#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
-static struct usbd_context *sample_usbd;
-
-static int enable_usb_device_next(void)
-{
-	int err;
-
-	sample_usbd = sample_usbd_init_device(NULL);
-	if (sample_usbd == NULL) {
-		return -ENODEV;
-	}
-
-	err = usbd_enable(sample_usbd);
-	if (err) {
-		return err;
-	}
-
-	return 0;
-}
-#endif /* defined(CONFIG_USB_DEVICE_STACK_NEXT) */
-
-int main(void)
-{
-	const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-	uint32_t dtr = 0;
-
-#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
-	if (enable_usb_device_next()) {
-		return 0;
-	}
-#else
-	if (usb_enable(NULL)) {
-		return 0;
-	}
-#endif
-
-	/* Poll if the DTR flag was set */
-	while (!dtr) {
-		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-		/* Give CPU resources to low priority threads. */
-		k_sleep(K_MSEC(100));
-	}
-
-	while (1) {
-		printk("Hello World! %s\n", CONFIG_ARCH);
-		k_sleep(K_SECONDS(1));
-	}
-}
+ #include <zephyr/kernel.h>
+ #include <zephyr/drivers/gpio.h>
+ 
+ /* 1000 msec = 1 sec */
+ #define SLEEP_TIME_MS   1000
+ 
+ /* The devicetree node identifier for the "led0" alias. */
+ #define LED0_NODE DT_ALIAS(led0)
+ 
+ /*
+  * A build error on this line means your board is unsupported.
+  * See the sample documentation for information on how to fix this.
+  */
+ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+ 
+ int main(void)
+ {
+	 int ret;
+ 
+	 if (!gpio_is_ready_dt(&led)) {
+		 return 0;
+	 }
+ 
+	 ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	 if (ret < 0) {
+		 return 0;
+	 }
+ 
+	 while (1) {
+		 ret = gpio_pin_toggle_dt(&led);
+		 if (ret < 0) {
+			 return 0;
+		 }
+		 printk("Hello World! %s\n", CONFIG_BOARD);
+		 k_msleep(SLEEP_TIME_MS);
+	 }
+	 return 0;
+ }
+ 
