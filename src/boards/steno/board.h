ca5b663982aeb036899c6003e05efbc7446e0c45
Tong tong <100080262+tongtongwang86@users.noreply.github.com>
2025-06-07 19:58:03 +0800

    changed led pin

--- src/boards/steno/board.h
+++ src/boards/steno/board.h
@@ -33,7 +33,7 @@
 /* LED
  *------------------------------------------------------------------*/
 #define LEDS_NUMBER       1
-#define LED_PRIMARY_PIN   _PINNUM(0, 15) // Blue
+#define LED_PRIMARY_PIN   _PINNUM(1, 10) // Blue
 #define LED_STATE_ON      1
 
 /*------------------------------------------------------------------*/
